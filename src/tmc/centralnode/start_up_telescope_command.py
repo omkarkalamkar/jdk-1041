7e0b4db0c65966fd748f73b8b863995b83fe7742
Apurva Patkar <apurva.ska@gmail.com>
2021-04-06 07:15:45 +0000

    AT1-824 Fix unit test cases

--- src/tmc/centralnode/start_up_telescope_command.py
+++ src/tmc/centralnode/start_up_telescope_command.py
@@ -142,7 +142,7 @@ class StartUpTelescope(SKABaseDevice.OnCommand):
 
         :raises: Devfailed exception if error occures while  executing On command on leaf node.
         """
-        # this_server = TangoServerHelper.get_instance()
+        this_server = TangoServerHelper.get_instance()
         try:
             tango_client.send_command(const.CMD_ON)
             log_msg = "ON command invoked successfully on {}".format(
