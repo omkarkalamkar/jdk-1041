aa803192efdc59cf1d72a05adad8dcf9309e62bc
Akash Karangale <akashk.ska@gmail.com>
2021-06-25 17:44:43 +0530

    AT1-908 - Added attribute commandInProgress on central node.

--- src/tmc/centralnode/central_node.py
+++ src/tmc/centralnode/central_node.py
@@ -11,7 +11,7 @@ of state and mode attributes defined by the SKA Control Model.
 # PROTECTED REGION ID(CentralNode.additionnal_import) ENABLED START #
 import threading
 # Tango imports
-from tango import DebugIt, AttrWriteType
+from tango import DebugIt, AttrWriteType, DevString
 from tango.server import run, attribute, command, device_property
 from tmc.common.tango_server_helper import TangoServerHelper
 
@@ -153,6 +153,12 @@ class CentralNode(SKABaseDevice):
         doc="Activity Message",
     )
 
+    commandInProgress = attribute(
+        dtype=DevString,
+        access=AttrWriteType.READ,
+        doc="commandInProgress attribute of Central Node.",
+    )
+
     # ---------------
     # General methods
     # ---------------
@@ -189,7 +195,7 @@ class CentralNode(SKABaseDevice):
             device.attr_map["subarray2HealthState"] = HealthState.UNKNOWN
             device.attr_map["subarray3HealthState"] = HealthState.UNKNOWN
             device.attr_map["telescopeHealthState"] = HealthState.UNKNOWN
-
+            device.attr_map["commandInProgress"] = ""
             device._health_state = HealthState.OK
             device._build_state = "{},{},{}".format(
                 release.name, release.version, release.description
@@ -278,6 +284,12 @@ class CentralNode(SKABaseDevice):
         self.update_attr_map("activityMessage", value)
         # PROTECTED REGION END #    //  CentralNode.activity_message_write
     
+    def read_commandInProgress(self):
+        # PROTECTED REGION ID(CentralNode.desired_telescope_state_read) ENABLED START #
+        """Internal construct of TANGO. Returns Desired Telescope State. """
+        return self.attr_map["commandInProgress"]
+        # PROTECTED REGION END #    //  CentralNode.activity_message_read
+
     def update_attr_map(self, attr, val):
         """
         This method updates attribute value in attribute map. Once a thread has acquired a lock,
--- src/tmc/centralnode/device_data.py
+++ src/tmc/centralnode/device_data.py
@@ -43,6 +43,7 @@ class DeviceData:
         self.resource_manager = None
         self.obs_state_aggregator = None
         self.check_resources = None
+        self.command_in_progress = ""
 
     @staticmethod
     def get_instance():
