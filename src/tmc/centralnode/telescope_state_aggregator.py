03f5967520b76c056d30ffcaad4cd1d6ab1e1982
Digvijay Mali <digvijay.ska@gmail.com>
2021-07-05 18:13:46 +0530

    AT1-912 Resolve review comments on MR

--- src/tmc/centralnode/telescope_state_aggregator.py
+++ src/tmc/centralnode/telescope_state_aggregator.py
@@ -46,7 +46,7 @@ class TelescopeStateAggregator(Aggregator):
             self.dish_master_fqdn = []
             dish_device_ids = [str(i).zfill(4) for i in range(1, 5)]
             for dish in range(0, len(dish_device_ids)):
-                self.dish_master_fqdn = self.dish_master_fqdn.append(f"{'mid_d'}{dish_device_ids[dish]}{'/elt/master'}")
+                self.dish_master_fqdn.append(f"{'mid_d'}{dish_device_ids[dish]}{'/elt/master'}")
             self.fqdn_device_telescope_state_list = [self.csp_master_fqdn, self.sdp_master_fqdn]
             self.logger.info(f"The 1st fqdn_device_telescope_state_list is: {self.fqdn_device_telescope_state_list}")
             self.fqdn_device_telescope_state_list = self.fqdn_device_telescope_state_list + list(self.dish_master_fqdn)
