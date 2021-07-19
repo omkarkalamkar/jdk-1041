6f4416dace477ffb4c02505d22e567c8c31a411c
Kalyani Thigale <kalyanit.ska@gmail.com>
2021-07-19 19:13:33 +0530

    AT1-965 Fix unit test cases

--- tests/input_validator_test.py
+++ tests/input_validator_test.py
@@ -4,105 +4,113 @@ import json
 
 # other imports
 from tmc.centralnode.input_validator import AssignResourceValidator
-from tmc.centralnode.exceptions import ResourceReassignmentError, ResourceNotPresentError
+from tmc.centralnode.exceptions import (
+    ResourceReassignmentError,
+    ResourceNotPresentError,
+)
 from tmc.centralnode.exceptions import SubarrayNotPresentError, InvalidJSONError
 from tmc.centralnode import const
 
 # Sample 'good' JSON
-sample_assign_resources_request = {
-    "subarrayID": 1,
-    "dish": {"receptorIDList": ["0002", "0001"]},
-    "sdp": {
-        "id": "sbi-mvp01-20200325-00001",
-        "max_length": 100.0,
-        "scan_types": [
-            {
-                "id": "science_A",
-                "coordinate_system": "ICRS",
-                "ra": "02:42:40.771",
-                "dec": "-00:00:47.84",
-                "channels": [
-                    {
-                        "count": 744,
-                        "start": 0,
-                        "stride": 2,
-                        "freq_min": 0.35e9,
-                        "freq_max": 0.368e9,
-                        "link_map": [[0, 0], [200, 1], [744, 2], [944, 3]],
-                    },
-                    {
-                        "count": 744,
-                        "start": 2000,
-                        "stride": 1,
-                        "freq_min": 0.36e9,
-                        "freq_max": 0.368e9,
-                        "link_map": [[2000, 4], [2200, 5]],
-                    },
-                ],
-            },
-            {
-                "id": "calibration_B",
-                "coordinate_system": "ICRS",
-                "ra": "12:29:06.699",
-                "dec": "02:03:08.598",
-                "channels": [
-                    {
-                        "count": 744,
-                        "start": 0,
-                        "stride": 2,
-                        "freq_min": 0.35e9,
-                        "freq_max": 0.368e9,
-                        "link_map": [[0, 0], [200, 1], [744, 2], [944, 3]],
-                    },
-                    {
-                        "count": 744,
-                        "start": 2000,
-                        "stride": 1,
-                        "freq_min": 0.36e9,
-                        "freq_max": 0.368e9,
-                        "link_map": [[2000, 4], [2200, 5]],
-                    },
-                ],
-            },
-        ],
-        "processing_blocks": [
-            {
-                "id": "pb-mvp01-20200325-00001",
-                "workflow": {
-                    "type": "realtime",
-                    "id": "vis_receive",
-                    "version": "0.1.0",
-                },
-                "parameters": {},
-            },
-            {
-                "id": "pb-mvp01-20200325-00002",
-                "workflow": {
-                    "type": "realtime",
-                    "id": "test_realtime",
-                    "version": "0.1.0",
-                },
-                "parameters": {},
-            },
-            {
-                "id": "pb-mvp01-20200325-00003",
-                "workflow": {"type": "batch", "id": "ical", "version": "0.1.0"},
-                "parameters": {},
-                "dependencies": [
-                    {"pb_id": "pb-mvp01-20200325-00001", "type": ["visibilities"]}
-                ],
-            },
-            {
-                "id": "pb-mvp01-20200325-00004",
-                "workflow": {"type": "batch", "id": "dpreb", "version": "0.1.0"},
-                "parameters": {},
-                "dependencies": [
-                    {"pb_id": "pb-mvp01-20200325-00003", "type": ["calibration"]}
-                ],
-            },
-        ],
-    },
-}
+# sample_assign_resources_request = {
+#     "subarrayID": 1,
+#     "dish": {"receptorIDList": ["0002", "0001"]},
+#     "sdp": {
+#         "id": "sbi-mvp01-20200325-00001",
+#         "max_length": 100.0,
+#         "scan_types": [
+#             {
+#                 "id": "science_A",
+#                 "coordinate_system": "ICRS",
+#                 "ra": "02:42:40.771",
+#                 "dec": "-00:00:47.84",
+#                 "channels": [
+#                     {
+#                         "count": 744,
+#                         "start": 0,
+#                         "stride": 2,
+#                         "freq_min": 0.35e9,
+#                         "freq_max": 0.368e9,
+#                         "link_map": [[0, 0], [200, 1], [744, 2], [944, 3]],
+#                     },
+#                     {
+#                         "count": 744,
+#                         "start": 2000,
+#                         "stride": 1,
+#                         "freq_min": 0.36e9,
+#                         "freq_max": 0.368e9,
+#                         "link_map": [[2000, 4], [2200, 5]],
+#                     },
+#                 ],
+#             },
+#             {
+#                 "id": "calibration_B",
+#                 "coordinate_system": "ICRS",
+#                 "ra": "12:29:06.699",
+#                 "dec": "02:03:08.598",
+#                 "channels": [
+#                     {
+#                         "count": 744,
+#                         "start": 0,
+#                         "stride": 2,
+#                         "freq_min": 0.35e9,
+#                         "freq_max": 0.368e9,
+#                         "link_map": [[0, 0], [200, 1], [744, 2], [944, 3]],
+#                     },
+#                     {
+#                         "count": 744,
+#                         "start": 2000,
+#                         "stride": 1,
+#                         "freq_min": 0.36e9,
+#                         "freq_max": 0.368e9,
+#                         "link_map": [[2000, 4], [2200, 5]],
+#                     },
+#                 ],
+#             },
+#         ],
+#         "processing_blocks": [
+#             {
+#                 "id": "pb-mvp01-20200325-00001",
+#                 "workflow": {
+#                     "type": "realtime",
+#                     "id": "vis_receive",
+#                     "version": "0.1.0",
+#                 },
+#                 "parameters": {},
+#             },
+#             {
+#                 "id": "pb-mvp01-20200325-00002",
+#                 "workflow": {
+#                     "type": "realtime",
+#                     "id": "test_realtime",
+#                     "version": "0.1.0",
+#                 },
+#                 "parameters": {},
+#             },
+#             {
+#                 "id": "pb-mvp01-20200325-00003",
+#                 "workflow": {"type": "batch", "id": "ical", "version": "0.1.0"},
+#                 "parameters": {},
+#                 "dependencies": [
+#                     {"pb_id": "pb-mvp01-20200325-00001", "type": ["visibilities"]}
+#                 ],
+#             },
+#             {
+#                 "id": "pb-mvp01-20200325-00004",
+#                 "workflow": {"type": "batch", "id": "dpreb", "version": "0.1.0"},
+#                 "parameters": {},
+#                 "dependencies": [
+#                     {"pb_id": "pb-mvp01-20200325-00003", "type": ["calibration"]}
+#                 ],
+#             },
+#         ],
+#     },
+# }
+
+assign_input_file = "command_AssignResources.json"
+path = join(dirname(__file__), "data", assign_input_file)
+with open(path, "r") as f:
+    assign_input_str = f.read()
 
 
 class TestAssignResourceValidator:
@@ -124,10 +132,8 @@ class TestAssignResourceValidator:
             self._test_receptor_id_list,
             "ska_mid/tm_leaf_node/d",
         )
-        output_config = input_validator.loads(
-            json.dumps(sample_assign_resources_request)
-        )
-        assert output_config == sample_assign_resources_request
+        output_config = input_validator.loads(json.dumps(assign_input_file))
+        assert output_config == assign_input_file
 
     def test_validate_wrong_subarray_id(self):
         """
@@ -136,8 +142,8 @@ class TestAssignResourceValidator:
         """
 
         # Set wrong subarray id.
-        input_json = sample_assign_resources_request
-        input_json["subarrayID"] = 99
+        input_json = assign_input_file
+        input_json["subarray_id"] = 99
 
         input_validator = AssignResourceValidator(
             self._test_subarray_list,
@@ -156,9 +162,9 @@ class TestAssignResourceValidator:
         value in the input string.
         """
 
-        input_json = sample_assign_resources_request
+        input_json = assign_input_file
         invalid_receptor_id_list = ["9999"]
-        input_json["dish"]["receptorIDList"] = invalid_receptor_id_list
+        input_json["dish"]["receptor_ids"] = invalid_receptor_id_list
 
         input_validator = AssignResourceValidator(
             self._test_subarray_list,
