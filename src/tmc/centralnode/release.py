1d2c00d06e02db8678227159e73d0f2f680d7f27
shraddhabajare <shraddhab.ska@gmail.com>
2021-07-19 11:29:42 +0000

    AT1-958 Revert the changes to publish the image on CAR Nexus

--- src/tmc/centralnode/release.py
+++ src/tmc/centralnode/release.py
@@ -9,8 +9,8 @@
 
 """Release information for Python Package"""
 
-name = """ska-tmc-mid-centralnode"""
-version = "0.2.5"
+name = """centralnode"""
+version = "0.2.3"
 version_info = version.split(".")
 description = """Central Node is a coordinator of the complete M&C system."""
 author = "Team NCRA"
