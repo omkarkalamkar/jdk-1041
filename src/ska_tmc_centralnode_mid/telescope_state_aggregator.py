a4e87e1928ebf5303f06fb3430e7a47943e6b5be
shraddhabajare <shraddhab.ska@gmail.com>
2021-07-29 11:45:46 +0000

    AT1-945 Update imaging attribute

--- src/ska_tmc_centralnode_mid/telescope_state_aggregator.py
+++ src/ska_tmc_centralnode_mid/telescope_state_aggregator.py
@@ -272,8 +272,10 @@ class TelescopeStateAggregator(Aggregator):
                     )
                     if unique_telescope_states == set([DevState.ON]):
                         self.logger.info("In ON telescope state")
+                        self.this_server.device.attr_map["imaging"] = ModesAvailability.available
+                        self.logger.info("imaging attr is:", self.this_server.device.attr_map["imaging"])
+                        #self.this_server.write_attr("imaging", ModesAvailability.available, False)
                         self.this_server.write_attr("telescopeState", DevState.ON, False)
-                        self.this_server.write_attr("imaging", ModesAvailability.available, False)
                         self.generate_telescope_state_log_msg(DevState.ON)
                     elif unique_telescope_states == set([DevState.OFF]):
                         self.logger.info("In OFF telescope state")
