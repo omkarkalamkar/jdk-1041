7b1d6b9212b6f626e123a24845070020aabfcdca
mesnehal <snehal.ska@gmail.com>
2021-09-22 14:37:47 +0530

    SS-82: Update unit test test_tmc_state

--- tests/unit/test_tmc_state.py
+++ tests/unit/test_tmc_state.py
@@ -258,3 +258,65 @@ def test_tmc_state_standby_only_events(tango_context):
     cm = create_cm_no_faulty_devices(tango_context, False, True)
     set_device_standby(devFactory, cm, 1.5)
     assert cm.component.tmc_op_state == tango.DevState.STANDBY
+
+def set_devices_on(cm, devFactory, expected_elapsed_time):
+    proxy = devFactory.get_device("ska_mid/tm_subarray_node/1")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    proxy = devFactory.get_device("ska_mid/tm_subarray_node/2")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    proxy = devFactory.get_device("ska_mid/tm_subarray_node/3")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    proxy = devFactory.get_device("ska_mid/tm_leaf_node/csp_subarray01")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    proxy = devFactory.get_device("ska_mid/tm_leaf_node/csp_subarray02")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    proxy = devFactory.get_device("ska_mid/tm_leaf_node/csp_subarray03")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    proxy = devFactory.get_device("ska_mid/tm_leaf_node/sdp_subarray01")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    proxy = devFactory.get_device("ska_mid/tm_leaf_node/sdp_subarray02")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    proxy = devFactory.get_device("ska_mid/tm_leaf_node/sdp_subarray03")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    proxy = devFactory.get_device("ska_mid/tm_leaf_node/csp_master")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    proxy = devFactory.get_device("ska_mid/tm_leaf_node/sdp_master")
+    proxy.SetDirectState(tango.DevState.ON)
+    assert proxy.State() == tango.DevState.ON
+    # wait for the propagations by event or polling
+    start_time = time.time()
+    elapsed_time = 0
+    while cm.component.tmc_op_state != tango.DevState.ON:
+        elapsed_time = time.time() - start_time
+        time.sleep(0.1)
+        if elapsed_time > TIMEOUT:
+            pytest.fail("Timeout occurred while executing the test")
+    assert elapsed_time < expected_elapsed_time
+
+def test_tmc_state_on(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(tango_context, True, True)
+    set_devices_on(cm, devFactory, 1.5)
+    assert cm.component.tmc_op_state == tango.DevState.ON
+
+def test_tmc_state_on_only_monitoring_loop(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(tango_context, True, False)
+    set_devices_on(cm, devFactory, 1.5)
+    assert cm.component.tmc_op_state == tango.DevState.ON
+
+def test_tmc_state_on_only_events(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(tango_context, False, True)
+    set_devices_on(cm, devFactory, 1.5)
+    assert cm.component.tmc_op_state == tango.DevState.ON
