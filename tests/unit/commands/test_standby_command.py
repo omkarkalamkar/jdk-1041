880c673ca536bb4f7a74748c089c87b2ea90ce0a
Apurva <apurva.ska@gmail.com>
2021-09-30 19:03:58 +0530

    ST-921 Unhappy tests for TelescopeOff and TelescopeStandby commands.

--- tests/unit/commands/test_standby_command.py
+++ tests/unit/commands/test_standby_command.py
@@ -1,6 +1,7 @@
 
 import time
 import pytest
+import mock
 from ska_tmc_centralnode_mid.manager.adapters import BaseAdapter, SubArrayAdapter, DishAdapter
 from tests.helper_adapter_factory import HelperAdapterFactory
 from ska_tango_base.commands import ResultCode
@@ -48,8 +49,8 @@ def test_telescope_standby_command(tango_context):
     logger.info("checked %s devices in %s", len(cm.checked_devices), elapsed_time)
 
     my_adapter_factory = HelperAdapterFactory()
-    on_command = TelescopeStandby(cm, cm.op_state_model, my_adapter_factory)
-    (result_code, _) = on_command.do()
+    standby_command = TelescopeStandby(cm, cm.op_state_model, my_adapter_factory)
+    (result_code, _) = standby_command.do()
     assert result_code == ResultCode.OK
     for adapter in my_adapter_factory.adapters:
         if isinstance(adapter, DishAdapter):
@@ -61,3 +62,41 @@ def test_telescope_standby_command(tango_context):
             continue
         
         adapter.proxy.TelescopeStandBy.assert_called() 
+
+def test_telescope_standby_command_fail_subarray(tango_context):
+    logger.info("%s", tango_context)
+    cm, start_time = create_cm()
+    elapsed_time = time.time() - start_time
+    logger.info("checked %s devices in %s", len(cm.checked_devices), elapsed_time)
+    my_adapter_factory = HelperAdapterFactory()
+
+    # include exception in TelescopeStandby command
+    failing_dev = "ska_mid/tm_subarray_node/1"
+
+    attrs = {'TelescopeStandBy.side_effect': Exception}
+    subarrayMock = mock.Mock(**attrs)
+    my_adapter_factory.get_or_create_adapter(failing_dev, proxy=subarrayMock)
+
+    standby_command = TelescopeStandby(cm, cm.op_state_model, my_adapter_factory)
+    (result_code, message) = standby_command.do()
+    assert result_code == ResultCode.FAILED
+    assert failing_dev in message
+
+def test_telescope_standby_command_fail_dish(tango_context):
+    logger.info("%s", tango_context)
+    cm, start_time = create_cm()
+    elapsed_time = time.time() - start_time
+    logger.info("checked %s devices in %s", len(cm.checked_devices), elapsed_time)
+    my_adapter_factory = HelperAdapterFactory()
+
+    # include exception in TelescopeStandby command
+    failing_dev = "mid_d0001/elt/master"
+
+    attrs = {'SetStandbyFPMode.side_effect': Exception}
+    dishMasterLeafMock = mock.Mock(**attrs)
+    my_adapter_factory.get_or_create_adapter(failing_dev, proxy=dishMasterLeafMock)
+
+    standby_command = TelescopeStandby(cm, cm.op_state_model, my_adapter_factory)
+    (result_code, message) = standby_command.do()
+    assert result_code == ResultCode.FAILED
+    assert failing_dev in message
