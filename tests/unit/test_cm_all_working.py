60d0b1a315ab61bcf6732414759ecd10902600e2
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2021-10-11 12:29:55 +0200

    ST-926 refactoring unit tests
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

--- tests/unit/test_cm_all_working.py
+++ tests/unit/test_cm_all_working.py
@@ -1,26 +1,11 @@
-import logging
 import time
 
 import pytest
-import tango
-from ska_tango_base.control_model import HealthState
 from ska_tango_base.obs.obs_device import SKAObsDevice
 
-from ska_tmc_centralnode_mid.central_node import CentralNode
-from ska_tmc_centralnode_mid.dev_factory import DevFactory
-from ska_tmc_centralnode_mid.manager.component_manager import (
-    CNComponentManager,
-)
 from ska_tmc_centralnode_mid.model.component import SubArrayDeviceInfo
-from ska_tmc_centralnode_mid.model.op_state_model import TMCOpStateModel
 from tests.helper_subarray_device import HelperSubArrayDevice
-from tests.settings import (
-    DEVICE_LIST,
-    SLEEP_TIME,
-    TIMEOUT,
-    count_faulty_devices,
-    logger,
-)
+from tests.settings import count_faulty_devices, create_cm, logger
 
 
 @pytest.fixture()
@@ -48,32 +33,8 @@ def devices_to_load():
     )
 
 
-def create_cm(p_monitoring_loop=True, p_event_receiver=True):
-    op_state_model = TMCOpStateModel(logger)
-    cm = CNComponentManager(
-        op_state_model,
-        logger=logger,
-        _monitoring_loop=p_monitoring_loop,
-        _event_receiver=p_event_receiver,
-    )
-    for dev in DEVICE_LIST:
-        cm.add_device(dev)
-    start_time = time.time()
-    num_devices = len(DEVICE_LIST)
-    if not p_monitoring_loop:
-        return cm, start_time
-    while num_devices != len(cm.checked_devices):
-        time.sleep(SLEEP_TIME)
-        elapsed_time = time.time() - start_time
-        if elapsed_time > TIMEOUT:
-            pytest.fail("Timeout occurred while executing the test")
-
-    return cm, start_time
-
-
 def test_all_working(tango_context):
     logger.info("%s", tango_context)
-    # import debugpy; debugpy.debug_this_thread()
     cm, start_time = create_cm()
     num_faulty = count_faulty_devices(cm)
     assert num_faulty == 0
--- tests/unit/test_telescope_state_off.py
+++ tests/unit/test_telescope_state_off.py
@@ -1,17 +1,17 @@
 import time
-from logging import debug
 
 import pytest
 import tango
-from ska_tango_base.control_model import HealthState
-from ska_tango_base.subarray import SKASubarray
-from test_cm_all_working import create_cm
-from test_telescope_startup import create_cm_no_faulty_devices
 
 from ska_tmc_centralnode_mid.dev_factory import DevFactory
 from tests.helper_state_device import HelperStateDevice
 from tests.helper_subarray_device import HelperSubArrayDevice
-from tests.settings import TIMEOUT, count_faulty_devices, logger
+from tests.settings import (
+    TIMEOUT,
+    create_cm_no_faulty_devices,
+    ensure_telescope_state,
+    set_devices_state,
+)
 
 
 @pytest.fixture()
@@ -39,44 +39,52 @@ def devices_to_load():
     )
 
 
-def set_devices_off(devFactory, cm, expected_elapsed_time):
-    proxy = devFactory.get_device("mid_csp/elt/master")
-    proxy.SetDirectState(tango.DevState.OFF)
-    assert proxy.State() == tango.DevState.OFF
-    proxy = devFactory.get_device("mid_sdp/elt/master")
-    proxy.SetDirectState(tango.DevState.OFF)
-    assert proxy.State() == tango.DevState.OFF
-    proxy = devFactory.get_device("mid_d0001/elt/master")
-    proxy.SetDirectState(tango.DevState.OFF)
-    assert proxy.State() == tango.DevState.OFF
-    # wait for the propagations by event or polling
-    start_time = time.time()
-    elapsed_time = 0
-    while cm.component.telescope_state != tango.DevState.OFF:
-        elapsed_time = time.time() - start_time
-        time.sleep(0.1)
-        if elapsed_time > TIMEOUT:
-            pytest.fail("Timeout occurred while executing the test")
-    assert elapsed_time < expected_elapsed_time
-
-
 def test_telescope_state_off(tango_context):
-    # import debugpy; debugpy.debug_this_thread()
-    devFactory = DevFactory()
     cm = create_cm_no_faulty_devices(tango_context, True, True)
-    set_devices_off(devFactory, cm, 1.5)
+    set_devices_state(
+        devices=[
+            "mid_csp/elt/master",
+            "mid_sdp/elt/master",
+            "mid_d0001/elt/master",
+        ],
+        devFactory=DevFactory(),
+        state=tango.DevState.OFF,
+        cm=cm,
+        expected_elapsed_time=1.5,
+    )
+    ensure_telescope_state(cm, tango.DevState.OFF, expected_elapsed_time=1.5)
     assert cm.component.telescope_state == tango.DevState.OFF
 
 
 def test_telescope_state_off_only_monitoring_loop(tango_context):
-    devFactory = DevFactory()
     cm = create_cm_no_faulty_devices(tango_context, True, False)
-    set_devices_off(devFactory, cm, 1.5)
+    set_devices_state(
+        devices=[
+            "mid_csp/elt/master",
+            "mid_sdp/elt/master",
+            "mid_d0001/elt/master",
+        ],
+        devFactory=DevFactory(),
+        state=tango.DevState.OFF,
+        cm=cm,
+        expected_elapsed_time=1.5,
+    )
+    ensure_telescope_state(cm, tango.DevState.OFF, expected_elapsed_time=1.5)
     assert cm.component.telescope_state == tango.DevState.OFF
 
 
 def test_telescope_state_off_only_events(tango_context):
-    devFactory = DevFactory()
     cm = create_cm_no_faulty_devices(tango_context, False, True)
-    set_devices_off(devFactory, cm, 2)
+    set_devices_state(
+        devices=[
+            "mid_csp/elt/master",
+            "mid_sdp/elt/master",
+            "mid_d0001/elt/master",
+        ],
+        devFactory=DevFactory(),
+        state=tango.DevState.OFF,
+        cm=cm,
+        expected_elapsed_time=2,
+    )
+    ensure_telescope_state(cm, tango.DevState.OFF, expected_elapsed_time=2)
     assert cm.component.telescope_state == tango.DevState.OFF
