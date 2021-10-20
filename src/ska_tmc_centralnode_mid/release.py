28c43928ae4052899cbf3a57579fbf1e2ebccca2
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2021-10-20 15:11:34 +0200

    ST-927 bump release
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

--- src/ska_tmc_centralnode_mid/release.py
+++ src/ska_tmc_centralnode_mid/release.py
@@ -10,7 +10,7 @@
 """Release information for Python Package"""
 
 name = """ska-tmc-centralnode-mid"""
-version = "0.3.0"
+version = "0.3.1"
 version_info = version.split(".")
 description = """Central Node is a coordinator of the complete M&C system."""
 author = "Team NCRA"
