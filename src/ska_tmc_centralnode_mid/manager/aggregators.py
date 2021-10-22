7dd2dceb04d0a03c533a26e9b812e5bee00d6375
Aditya Dange <adityadange.ska@gmail.com>
2021-10-22 06:51:58 +0000

    ST-995 Added telescope state aggregator for low.

--- src/ska_tmc_centralnode_mid/manager/aggregators.py
+++ src/ska_tmc_centralnode_mid/manager/aggregators.py
@@ -73,6 +73,67 @@ class TelescopeStateAggragator(Aggregator):
             return DevState.UNKNOWN
 
 
+class TelescopeStateAggragatorLow(Aggregator):
+    def __init__(self, cm, logger) -> None:
+        super().__init__(cm, logger)
+
+    def aggregate(self):
+        # Currently there is only MCCS in the Low. But this algorithm leaves a
+        #  place to consider CSP and SDP states when they will be integrated.
+        telescopeStateList = []
+        mccs_master = False
+
+        for dev in self._component_manager.checked_devices:
+            name = dev.dev_name.lower()
+            if dev.faulty:
+                continue
+            elif (
+                name
+                == self._component_manager.input_parameter.mccs_master_dev_name
+            ):
+                telescopeStateList.append(dev.state)
+                mccs_master = True
+            # TODO: Enable this block when CSP and SDP are integrated
+            # elif (
+            #     name
+            #     == self._component_manager.input_parameter.csp_master_dev_name
+            # ):
+            #   telescopeStateList.append(dev.state)
+            #   csp_master = True
+            # elif (
+            #     name
+            #     == self._component_manager.input_parameter.sdp_master_dev_name
+            # ):
+            #   telescopeStateList.append(dev.state)
+            #   sdp_master = True
+
+        telescopeSetStateList = set(telescopeStateList)
+        # TODO: Enable this block when CSP and SDP are integrated
+        # if not sdp_master and not csp_master:
+        #     self._logger.info(
+        #         "missing devices: %s=%s %s=%s",
+        #         self._component_manager.input_parameter.sdp_master_dev_name,
+        #         sdp_master,
+        #         self._component_manager.input_parameter.csp_master_dev_name,
+        #         csp_master,
+        #     )
+        #     return DevState.UNKNOWN
+        if not mccs_master:
+            return DevState.UNKNOWN
+        elif telescopeSetStateList == set([DevState.ON]):
+            return DevState.ON
+        elif telescopeSetStateList == set([DevState.OFF]):
+            return DevState.OFF
+        elif DevState.INIT in telescopeSetStateList:
+            return DevState.INIT
+        elif DevState.FAULT in telescopeSetStateList:
+            return DevState.FAULT
+        elif DevState.STANDBY in telescopeSetStateList:
+            return DevState.STANDBY
+        else:
+            return DevState.UNKNOWN
+
+
 class HealthStateAggragator(Aggregator):
     def __init__(self, cm, logger) -> None:
         super().__init__(cm, logger)
