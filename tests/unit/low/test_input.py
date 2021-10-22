ea5d0e335b7b86b38101abec2070c35664c98265
Matteo Di Carlo <matteo.dicarlo@inaf.it>
2021-10-22 16:18:43 +0200

    ST-995 test input for low
    Signed-off-by: Matteo Di Carlo <matteo.dicarlo@inaf.it>

---
+++ tests/unit/low/test_input.py
@@ -0,0 +1,11 @@
+from ska_tmc_centralnode_mid.model.input import InputParameterLow
+
+
+def test_properties():
+    input = InputParameterLow(None)
+    input.tm_subarray_dev_names = ("1", "2")
+    assert input.tm_subarray_dev_names == ("1", "2")
+    input.mccs_master_leaf_node = "leaf node"
+    assert input.mccs_master_leaf_node == "leaf node"
+    input.mccs_master_dev_name = "master node"
+    assert input.mccs_master_dev_name == "master node"
