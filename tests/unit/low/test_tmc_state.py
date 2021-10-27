d1ab1c34ba8f8f82991e154027a13e9899eb4225
shraddhabajare <shraddhab.ska@gmail.com>
2021-10-27 19:00:37 +0530

    ST-997 - CentralNodeLow: Add unit test cases for OpState aggregator

---
+++ tests/unit/low/test_tmc_state.py
@@ -0,0 +1,120 @@
+import pytest
+import tango
+
+from ska_tmc_centralnode_mid.dev_factory import DevFactory
+from ska_tmc_centralnode_mid.model.input import InputParameterLow
+from tests.helper_state_device import HelperStateDevice
+from tests.helper_subarray_device import HelperSubArrayDevice
+from tests.settings import (
+    create_cm_no_faulty_devices,
+    ensure_tmc_op_state,
+    set_device_state,
+)
+
+
+@pytest.fixture()
+def devices_to_load():
+    return (
+        {
+            "class": HelperSubArrayDevice,
+            "devices": [
+                {"name": "ska_low/tm_subarray_node/1"},
+                {"name": "ska_low/tm_leaf_node/mccs_subarray01"},
+            ],
+        },
+        {
+            "class": HelperStateDevice,
+            "devices": [
+                {"name": "ska_low/tm_leaf_node/mccs_master"},
+                {"name": "low-mccs/control/control"},
+            ],
+        },
+    )
+
+
+def set_device_init(devFactory, cm, expected_elapsed_time):
+    set_device_state(
+        "ska_low/tm_subarray_node/1", tango.DevState.INIT, devFactory
+    )
+    ensure_tmc_op_state(cm, tango.DevState.INIT, expected_elapsed_time)
+
+
+def test_tmc_state_init(tango_context):
+    # import debugpy; debugpy.debug_this_thread()
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(
+        tango_context, True, True, InputParameterLow(None)
+    )
+    set_device_init(devFactory, cm, 1.5)
+    assert cm.component.tmc_op_state == tango.DevState.INIT
+
+
+def test_tmc_state_init_only_monitoring_loop(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(
+        tango_context, True, False, InputParameterLow(None)
+    )
+    set_device_init(devFactory, cm, 1.5)
+    assert cm.component.tmc_op_state == tango.DevState.INIT
+
+
+def test_tmc_state_init_only_events(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(
+        tango_context, False, True, InputParameterLow(None)
+    )
+    set_device_init(devFactory, cm, 2)
+    assert cm.component.tmc_op_state == tango.DevState.INIT
+
+
+def set_one_device_fault(devFactory, cm, expected_elapsed_time):
+    set_device_state(
+        "ska_low/tm_subarray_node/1", tango.DevState.FAULT, devFactory
+    )
+    set_device_state(
+        "ska_low/tm_leaf_node/mccs_subarray01", tango.DevState.OFF, devFactory
+    )
+    set_device_state(
+        "ska_low/tm_leaf_node/mccs_master", tango.DevState.STANDBY, devFactory
+    )
+    ensure_tmc_op_state(cm, tango.DevState.FAULT, expected_elapsed_time)
+
+
+def test_tmc_state_fault_over_standby(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(
+        tango_context, True, True, InputParameterLow(None)
+    )
+    set_one_device_fault(devFactory, cm, 2)
+    assert cm.component.tmc_op_state == tango.DevState.FAULT
+
+
+def test_tmc_state_fault_over_standby_only_monitoring_loop(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(
+        tango_context, True, False, InputParameterLow(None)
+    )
+    set_one_device_fault(devFactory, cm, 2)
+    assert cm.component.tmc_op_state == tango.DevState.FAULT
+
+
+def test_tmc_state_fault_over_standby_only_events(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(
+        tango_context, False, True, InputParameterLow(None)
+    )
+    set_one_device_fault(devFactory, cm, 2)
+    assert cm.component.tmc_op_state == tango.DevState.FAULT
+
+
+def set_device_standby(devFactory, cm, expected_elapsed_time):
+    set_device_state(
+        "ska_low/tm_subarray_node/1", tango.DevState.STANDBY, devFactory
+    )
+    set_device_state(
+        "ska_low/tm_leaf_node/mccs_subarray01", tango.DevState.OFF, devFactory
+    )
+    set_device_state(
+        "ska_low/tm_leaf_node/mccs_master", tango.DevState.ON, devFactory
+    )
+    ensure_tmc_op_state(cm, tango.DevState.STANDBY, expected_elapsed_time)
---
+++ tests/unit/low/test_tmc_state_on.py
@@ -0,0 +1,74 @@
+import pytest
+import tango
+
+from ska_tmc_centralnode_mid.dev_factory import DevFactory
+from ska_tmc_centralnode_mid.model.input import InputParameterLow
+from tests.helper_state_device import HelperStateDevice
+from tests.helper_subarray_device import HelperSubArrayDevice
+from tests.settings import (
+    create_cm_no_faulty_devices,
+    ensure_tmc_op_state,
+    set_devices_state,
+)
+
+
+@pytest.fixture()
+def devices_to_load():
+    return (
+        {
+            "class": HelperSubArrayDevice,
+            "devices": [
+                {"name": "ska_low/tm_subarray_node/1"},
+                {"name": "ska_low/tm_leaf_node/mccs_subarray01"},
+            ],
+        },
+        {
+            "class": HelperStateDevice,
+            "devices": [
+                {"name": "ska_low/tm_leaf_node/mccs_master"},
+                {"name": "low-mccs/control/control"},
+            ],
+        },
+    )
+
+
+def set_devices_on(cm, devFactory, expected_elapsed_time):
+    set_devices_state(
+        devices=[
+            "ska_low/tm_subarray_node/1",
+            "ska_low/tm_leaf_node/mccs_subarray01",
+            "ska_low/tm_leaf_node/mccs_master",
+        ],
+        devFactory=devFactory,
+        state=tango.DevState.ON,
+        cm=cm,
+        expected_elapsed_time=expected_elapsed_time,
+    )
+    ensure_tmc_op_state(cm, tango.DevState.ON, expected_elapsed_time)
+
+
+def test_tmc_state_on(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(
+        tango_context, True, True, InputParameterLow(None)
+    )
+    set_devices_on(cm, devFactory, 1.5)
+    assert cm.component.tmc_op_state == tango.DevState.ON
+
+
+def test_tmc_state_on_only_monitoring_loop(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(
+        tango_context, True, False, InputParameterLow(None)
+    )
+    set_devices_on(cm, devFactory, 1.5)
+    assert cm.component.tmc_op_state == tango.DevState.ON
+
+
+def test_tmc_state_on_only_events(tango_context):
+    devFactory = DevFactory()
+    cm = create_cm_no_faulty_devices(
+        tango_context, False, True, InputParameterLow(None)
+    )
+    set_devices_on(cm, devFactory, 1.5)
+    assert cm.component.tmc_op_state == tango.DevState.ON
