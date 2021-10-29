16c4d7745c330a0b840042f1bfc455c86f0dbebb
shraddhabajare <shraddhab.ska@gmail.com>
2021-10-29 16:12:14 +0530

    ST-998 - Fix k8s-test-low job

--- tests/integration/test_assign_res_command.py
+++ tests/integration/test_assign_res_command.py
@@ -93,8 +93,7 @@ def test_assign_res_command_low(tango_context, central_node_name):
                 dirname(__file__),
                 "..",
                 "data",
-                "low",
-                "command_AssignResources.json",
+                "command_mccs_AssignResources.json",
             )
         ),
     )
