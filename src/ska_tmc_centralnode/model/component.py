f1101af10615f9b2035388708d762a9ac0a82bc1
Apurva <apurva.ska@gmail.com>
2021-11-12 19:51:20 +0530

    ST-1013 Bump the CentralNode version to 0.3.4

--- src/ska_tmc_centralnode/model/component.py
+++ src/ska_tmc_centralnode/model/component.py
@@ -233,7 +233,6 @@ class Component:
         :param value: the new telescope health state
         :type value: HealthState
         """
-        # if isinstance(value, HealthState):
         if self._telescope_health_state != value:
             self._telescope_health_state = value
             self._invoke_telescope_health_state_callback()
