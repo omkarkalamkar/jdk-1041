e7cae6aa39127b034f2a92e6b5cb5e29ca4264ec
akshaysonawane <akshayss.ska@gmail.com>
2022-04-26 13:28:12 +0530

    AT1-1039 - Resolve integration test errors

--- tests/unit/commands/low/test_standby_command.py
+++ tests/unit/commands/low/test_standby_command.py
@@ -17,10 +17,6 @@ from ska_tmc_centralnode.commands.telescope_standby_command import (
     TelescopeStandby,
 )
 from ska_tmc_centralnode.model.input import InputParameterLow
-
-# from tests.helpers.helper_adapter_factory import HelperAdapterFactory
-# from tests.helpers.helper_state_mccsdevice import HelperMCCSStateDevice
-# from tests.helpers.helper_subarray_device import HelperSubArrayDevice
 from tests.settings import create_cm, logger
 
 
