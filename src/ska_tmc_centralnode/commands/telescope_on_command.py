20feec76b41b9bb66be8cab37dfa805e5c21eb17
user <jayant.ska@gmail.com>
2022-05-06 15:52:58 +0530

    AT1-1131 - Opimize TelescopeOn.do_mid() code

--- src/ska_tmc_centralnode/commands/telescope_on_command.py
+++ src/ska_tmc_centralnode/commands/telescope_on_command.py
@@ -1,3 +1,5 @@
+import operator
+
 from ska_tango_base.commands import ResultCode
 from tango import DevState
 
@@ -45,50 +47,74 @@ class TelescopeOn(AbstractTelescopeOnOff):
 
         component_manager.component.desired_telescope_state = DevState.ON
 
-        # send commands to sub-devices
-        # import debugpy; debugpy.debug_this_thread()
-        try:
-            self.tm_leaf_csp_master_adapter.On()
-        except Exception as e:
-            return self.generate_command_result(
-                ResultCode.FAILED,
-                f"Error in calling Telescope On in TM CSP Master Leaf {self.tm_leaf_csp_master_adapter.dev_name}: {e}",
-            )
-
-        try:
-            self.tm_leaf_sdp_master_adapter.On()
-        except Exception as e:
-            return self.generate_command_result(
-                ResultCode.FAILED,
-                f"Error in calling Telescope On in TM SDP Master Leaf {self.tm_leaf_sdp_master_adapter.dev_name}: {e}",
-            )
-
-        for adapter in self.tm_subarray_adapters:
-            try:
-                adapter.On()
-            except Exception as e:
-                return self.generate_command_result(
-                    ResultCode.FAILED,
-                    f"Error in calling Telescope On in TM Subarray {adapter.dev_name}: {e}",
-                )
-
-        for adapter in self.tm_dish_adapters:
-            try:
-                adapter.SetStandbyFPMode()
-            except Exception as e:
-                return self.generate_command_result(
-                    ResultCode.FAILED,
-                    f"Error in calling SetStandbyFPMode in TM Dish Leaf {adapter.dev_name}: {e}",
-                )
-            try:
-                adapter.SetOperateMode()
-            except Exception as e:
-                return self.generate_command_result(
-                    ResultCode.FAILED,
-                    f"Error in calling SetOperateMode in TM Dish Leaf {adapter.dev_name}: {e}",
-                )
+        for ret_code, message in [
+            self.turn_on_csp(),
+            self.turn_on_sdp(),
+            self.turn_on_subarrays(),
+            self.set_standby_fp_mode_dishes(),
+            self.set_operate_mode_dishes(),
+        ]:
+            if ret_code == ResultCode.FAILED:
+                return ret_code, message
 
         return (ResultCode.OK, "")
+
+    # This method will be moved to abstract class
+    def invoke_command(
+        self,
+        adapters: list,
+        command_caller,
+        description: str,
+    ):
+        try:
+            for adapter in adapters:
+                command_caller(adapter)
+        except Exception as e:
+            return self.generate_command_result(
+                ResultCode.FAILED,
+                f"{description} {adapter.dev_name}: {e}",
+            )
+        return (ResultCode.OK, "")
+
+    def send_command(self, adapters, description, command):
+        return self.invoke_command(
+            adapters, operator.methodcaller(command), description
+        )
+
+    def turn_on_sdp(self):
+        return self.send_command(
+            [self.tm_leaf_sdp_master_adapter],
+            "Error in calling TelescopeOn() on TMC SDP Subarray leaf",
+            "On",
+        )
+
+    def turn_on_csp(self):
+        return self.send_command(
+            [self.tm_leaf_csp_master_adapter],
+            "Error in calling TelescopeOn() on TMC CSP Subarray leaf",
+            "On",
+        )
+
+    def turn_on_subarrays(self):
+        return self.send_command(
+            self.tm_subarray_adapters,
+            "Error in calling TelescopeOn() on TMC Subarray",
+            "On",
+        )
+
+    def set_standby_fp_mode_dishes(self):
+        return self.send_command(
+            self.tm_dish_adapters,
+            "Error in calling TelescopeOn() on TMC Dish leaf node",
+            "SetStandbyFPMode",
+        )
+
+    def set_operate_mode_dishes(self):
+        return self.send_command(
+            self.tm_dish_adapters,
+            "Error in calling TelescopeOn() on TMC Dish leaf node",
+            "SetOperateMode",
+        )
 
     def do_low(self, argin=None):
         """
--- tests/unit/commands/mid/test_on_command.py
+++ tests/unit/commands/mid/test_on_command.py
@@ -53,13 +53,13 @@ def test_telescope_on_command(tango_context):
     for adapter in my_adapter_factory.adapters:
         if isinstance(adapter, DishAdapter):
             adapter.proxy.SetStandbyFPMode.assert_called()
-            adapter.proxy.SetOperateMode.assert_called()
+            adapter.proxy.SetOperateMode.assert_called_once_with()
             continue
         if isinstance(adapter, SubArrayAdapter):
-            adapter.proxy.On.assert_called()
+            adapter.proxy.On.assert_called_once_with()
             continue
 
-        adapter.proxy.On.assert_called()
+        adapter.proxy.On.assert_called_once_with()
 
 
 def test_telescope_on_command_fail_subarray(tango_context):
