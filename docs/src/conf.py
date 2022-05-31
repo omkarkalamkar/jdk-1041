3112d095083412d48eef9558fc4ebde0df0391fb
akshaysonawane <akshayss.ska@gmail.com>
2022-05-31 11:01:13 +0530

    AT1-1127 - Updated docs file to resolve issue in documentation

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -39,7 +39,10 @@ autodoc_mock_imports = [
     "ValidationError",
     "ska_telmodel",
     "ska_ser_skuid",
-    "transitions"
+    "transitions",
+    "ska_tmc_common",
+    "ska_ser_log_transactions"
+
 ]
 
 import os
