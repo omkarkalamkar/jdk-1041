5e702f3954343272030c6f529b64edcb7db4df2f
shraddhabajare <shraddhab.ska@gmail.com>
2022-07-17 20:07:27 +0530

    SAH-1149 - Enable TelescopeOn integration test case. Analyse the behaviour

--- tests/integration/test_on_command.py
+++ tests/integration/test_on_command.py
@@ -16,26 +16,41 @@ def on_command(tango_context, centralnode_name):
     dev_factory = DevFactory()
     central_node = dev_factory.get_device(centralnode_name)
     ensure_checked_devices(central_node)
-    initial_len = len(central_node.commandExecuted)
-    (result, unique_id) = central_node.On()
-    logger.info(result)
-    logger.info(unique_id)
+    logger.info(
+        f"Before TelescopeOn longRunningCommandsInQueue attribute value is:::{central_node.longRunningCommandsInQueue}"
+    )
+    result, unique_id = central_node.TelescopeOn()
+    logger.info(f"Command ID: {unique_id} Returned result: {result}")
+    logger.info(
+        f"After TelescopeOn longRunningCommandIDsInQueue attribute value is:::{central_node.longRunningCommandIDsInQueue}"
+    )
     assert result[0] == ResultCode.QUEUED
-    start_time = time.time()
-    while len(central_node.commandExecuted) != initial_len + 1:
-        time.sleep(SLEEP_TIME)
-        elapsed_time = time.time() - start_time
-        if elapsed_time > TIMEOUT:
-            pytest.fail("Timeout occurred while executing the test")
+    logger.info(
+        f"After TelescopeOn longRunningCommandsInQueue attribute value is:::{central_node.longRunningCommandsInQueue}"
+    )
+    command_id, result = central_node.longRunningCommandResult
+    logger.info(
+        f"After TelescopeOn central_node.longRunningCommandResult:::::::{central_node.longRunningCommandResult}"
+    )
+    if command_id == unique_id[0]:
+        logger.info(f"command:::::::{command_id}")
+        assert result == "0"
 
-    for command in central_node.commandExecuted:
-        if command[0] == unique_id[0]:
-            assert command[2] == "ResultCode.OK"
+    logger.info(
+        f"After TelescopeOn central_node.telescopeState:::::::{central_node.telescopeState}"
+    )
+
+    sdp_master = dev_factory.get_device("mid_sdp/elt/master")
+    csp_master = dev_factory.get_device("mid-csp/elt/master")
+    dish_master1 = dev_factory.get_device("mid_d0001/elt/master")
+
+    logger.info(f"After TelescopeOn sdp_master.State:::::::{sdp_master.State}")
 
 
-@pytest.mark.xfail(
-    reason="Test needs update as per v0.13. Can be done as a part of further commands refactoring."
-)
+@pytest.mark.ncra
+# @pytest.mark.xfail(
+#     reason="Test needs update as per v0.13. Can be done as a part of further commands refactoring."
+# )
 @pytest.mark.post_deployment
 @pytest.mark.SKA_mid
 def test_on_command_mid(tango_context):
