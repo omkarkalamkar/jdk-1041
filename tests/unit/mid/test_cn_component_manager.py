efe7eabf75e05de36c2aea7ddc27ce8c299f41c0
shraddhabajare <shraddhab.ska@gmail.com>
2022-07-20 19:17:47 +0530

    SAH-1149 - Make testcase pass

--- tests/unit/mid/test_cn_component_manager.py
+++ tests/unit/mid/test_cn_component_manager.py
@@ -1,15 +1,17 @@
 import pytest
 from ska_tango_base.executor import TaskStatus
+from ska_tmc_common.op_state_model import TMCOpStateModel
 
 from ska_tmc_centralnode.manager.component_manager import CNComponentManager
 from ska_tmc_centralnode.model.input import InputParameterMid
 from tests.settings import logger
 
 
-@pytest.mark.refactor_telescopeon
+@pytest.mark.letsmakeitpass
 def test_telescope_on():
+    op_state_model = TMCOpStateModel(logger)
     cm = CNComponentManager(
-        logger=logger, _input_parameter=InputParameterMid(None)
+        op_state_model, logger=logger, _input_parameter=InputParameterMid(None)
     )
     res_code, message = cm.telescope_on()
     assert res_code == TaskStatus.QUEUED
