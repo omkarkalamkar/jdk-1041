ea6c31faf40413914d5dbe1d37f91fec3de37c34
shraddhabajare <shraddhab.ska@gmail.com>
2022-07-20 16:59:41 +0530

    SAH-1149 - Fix unit test case for TelescopeOn command class

--- tests/settings.py
+++ tests/settings.py
@@ -47,7 +47,7 @@ def count_faulty_devices(cm):
 
 
 def create_cm(
-    p_liveliness_probe=True,
+    p_liveliness_probe=False,
     p_event_receiver=True,
     input_parameter=InputParameterMid(None),
 ):
