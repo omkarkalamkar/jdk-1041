112612271384930cc29e1d32f8dbbfeba1e399fb
shraddhabajare <shraddhab.ska@gmail.com>
2022-07-25 16:25:10 +0530

    SAH-1149 - Resolve pylint warnings

--- src/ska_tmc_centralnode/central_node.py
+++ src/ska_tmc_centralnode/central_node.py
@@ -6,7 +6,7 @@ of state and mode attributes defined by the SKA Control Model.
 import json
 
 import pandas as pd
-from ska_tango_base.commands import ResultCode, SubmittedSlowCommand
+from ska_tango_base.commands import ResultCode
 from ska_tango_base.control_model import HealthState
 from ska_tmc_common.op_state_model import TMCOpStateModel
 from ska_tmc_common.tmc_base_device import TMCBaseDevice
