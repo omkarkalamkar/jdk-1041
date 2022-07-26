9704efaaae13800ef09da8de8d0a15e2d02d77a4
shraddhabajare <shraddhab.ska@gmail.com>
2022-07-26 15:03:54 +0530

    SAH-1149 - Enable tmc_op_state and telescope_state attribute tests

--- tests/unit/mid/test_telescope_state.py
+++ tests/unit/mid/test_telescope_state.py
@@ -43,7 +43,7 @@ def set_device_init(devFactory, cm, expected_elapsed_time):
     ensure_telescope_state(cm, tango.DevState.INIT, expected_elapsed_time)
 
 
-@pytest.mark.skip(reason="Stepwise tests execution on pipeline")
+# @pytest.mark.skip(reason="Stepwise tests execution on pipeline")
 def test_telescope_state_init(tango_context):
     devFactory = DevFactory()
     cm = create_cm_no_faulty_devices(tango_context, True, True)
@@ -60,7 +60,7 @@ def set_one_device_fault(devFactory, cm, expected_elapsed_time):
     ensure_telescope_state(cm, tango.DevState.FAULT, expected_elapsed_time)
 
 
-@pytest.mark.skip(reason="Stepwise tests execution on pipeline")
+# @pytest.mark.skip(reason="Stepwise tests execution on pipeline")
 def test_telescope_state_fault_over_standby(tango_context):
     devFactory = DevFactory()
     cm = create_cm_no_faulty_devices(tango_context, True, True)
@@ -75,7 +75,7 @@ def set_device_standby(devFactory, cm, expected_elapsed_time):
     ensure_telescope_state(cm, tango.DevState.STANDBY, expected_elapsed_time)
 
 
-@pytest.mark.skip(reason="Stepwise tests execution on pipeline")
+# @pytest.mark.skip(reason="Stepwise tests execution on pipeline")
 def test_telescope_state_standby(tango_context):
     devFactory = DevFactory()
     cm = create_cm_no_faulty_devices(tango_context, True, True)
--- tests/unit/mid/test_tmc_state.py
+++ tests/unit/mid/test_tmc_state.py
@@ -43,7 +43,7 @@ def set_device_init(devFactory, cm, expected_elapsed_time):
     ensure_tmc_op_state(cm, tango.DevState.INIT, expected_elapsed_time)
 
 
-@pytest.mark.skip(reason="Stepwise tests execution on pipeline")
+# @pytest.mark.skip(reason="Stepwise tests execution on pipeline")
 def test_tmc_state_init(tango_context):
     devFactory = DevFactory()
     cm = create_cm_no_faulty_devices(tango_context, True, True)
@@ -70,7 +70,7 @@ def set_one_device_fault(devFactory, cm, expected_elapsed_time):
     ensure_tmc_op_state(cm, tango.DevState.FAULT, expected_elapsed_time)
 
 
-@pytest.mark.skip(reason="Stepwise tests execution on pipeline")
+# @pytest.mark.skip(reason="Stepwise tests execution on pipeline")
 def test_tmc_state_fault_over_standby(tango_context):
     devFactory = DevFactory()
     cm = create_cm_no_faulty_devices(tango_context, True, True)
