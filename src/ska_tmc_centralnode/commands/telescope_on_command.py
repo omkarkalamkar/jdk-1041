eea3d3037fe1a151c0fcf9bd336fda8e48eb758a
shraddhabajare <shraddhab.ska@gmail.com>
2022-07-26 17:16:17 +0530

    SAH-1149 - Utilise ska-tmc-common v0.1.37

--- src/ska_tmc_centralnode/commands/telescope_on_command.py
+++ src/ska_tmc_centralnode/commands/telescope_on_command.py
@@ -33,7 +33,6 @@ class TelescopeOn(AbstractTelescopeOnOff):
         )
         self._timeout_mccs = timeout_mccs
         self._step_sleep = step_sleep
-        # self.init_adapters()
 
     def telescope_on(
         self,
@@ -42,7 +41,8 @@ class TelescopeOn(AbstractTelescopeOnOff):
         task_abort_event: Optional[threading.Event] = None,
     ):
 
-        """This is a long running method
+        """This is a long running method for TelecopeOn command, it executes do hook,
+        invokes TelescopeOn command on lowe level devices.
 
         :param logger: logger
         :type logger: logging.Logger
@@ -52,7 +52,6 @@ class TelescopeOn(AbstractTelescopeOnOff):
         :type task_abort_event: Event, optional
         """
         # Indicate that the task has started
-        # if task_callback:
         task_callback(status=TaskStatus.IN_PROGRESS)
 
         ret_code, message = self.do(argin=None)
@@ -96,28 +95,25 @@ class TelescopeOn(AbstractTelescopeOnOff):
             if ret_code == ResultCode.FAILED:
                 return ret_code, message
         self.logger.info(
-            "do_mid for TelescopeOn command on the lower level devices is successful"
+            "TelescopeOn command is invoked successfully on the lower level devices"
         )
         return (ResultCode.OK, "")
 
     def turn_on_sdp(self):
-        self.logger.info("TelescopeOn for Sdp devices")
         return self.send_command(
             [self.tm_leaf_sdp_master_adapter],
-            f"Error in calling On() command on {self.tm_leaf_sdp_master_adapter}",
+            f"Error in calling On() command on {self.tm_leaf_sdp_master_adapter.dev_name}",
             "On",
         )
 
     def turn_on_csp(self):
-        self.logger.info("TelescopeOn for Csp devices")
         return self.send_command(
             [self.tm_leaf_csp_master_adapter],
-            f"Error in calling On() command on {self.tm_leaf_csp_master_adapter}",
+            f"Error in calling On() command on {self.tm_leaf_csp_master_adapter.dev_name}",
             "On",
         )
 
     def turn_on_subarrays(self):
-        self.logger.info("TelescopeOn for tm subarrays devices")
         return self.send_command(
             self.tm_subarray_adapters,
             f"Error in calling On() command on {self.tm_subarray_adapters}",
@@ -125,7 +121,6 @@ class TelescopeOn(AbstractTelescopeOnOff):
         )
 
     def set_standby_fp_mode_dishes(self):
-        self.logger.info("TelescopeOn for dish devices")
         return self.send_command(
             self.tm_dish_adapters,
             f"Error in calling SetStandbyFPMode() command on {self.tm_dish_adapters}",
@@ -133,7 +128,6 @@ class TelescopeOn(AbstractTelescopeOnOff):
         )
 
     def set_operate_mode_dishes(self):
-        self.logger.info("TelescopeOn for dish devices")
         return self.send_command(
             self.tm_dish_adapters,
             f"Error in calling SetOperateMode() command on {self.tm_dish_adapters}",
@@ -164,6 +158,6 @@ class TelescopeOn(AbstractTelescopeOnOff):
     def turn_on_mccs_master(self):
         return self.send_command(
             [self.tm_leaf_mccs_master_adapter],
-            f"Error in calling On() command for {self.tm_leaf_mccs_master_adapter}",
+            f"Error in calling On() command on {self.tm_leaf_mccs_master_adapter.dev_name}",
             "On",
         )
--- src/ska_tmc_centralnode/manager/component_manager.py
+++ src/ska_tmc_centralnode/manager/component_manager.py
@@ -24,7 +24,7 @@ from ska_tmc_centralnode.manager.aggregators import (
     TMCOpStateAggregator,
 )
 
-# from ska_tmc_centralnode.manager.event_receiver import CentralNodeEventReceiver
+from ska_tmc_centralnode.manager.event_receiver import CentralNodeEventReceiver
 from ska_tmc_centralnode.model.component import CentralComponent
 from ska_tmc_centralnode.model.enum import ModesAvailability
 from ska_tmc_centralnode.model.input import (
@@ -96,15 +96,15 @@ class CNComponentManager(TmcComponentManager):
         self.op_state_model = op_state_model
         self.adapter_factory = AdapterFactory()
 
-        # if self.event_receiver:
-        #     self.event_receiver_object = CentralNodeEventReceiver(
-        #         self,
-        #         logger=self.logger,
-        #         proxy_timeout=self.proxy_timeout,
-        #         sleep_time=self.sleep_time,
-        #     )
+        if self.event_receiver:
+            self.event_receiver_object = CentralNodeEventReceiver(
+                self,
+                logger=self.logger,
+                proxy_timeout=self.proxy_timeout,
+                sleep_time=self.sleep_time,
+            )
 
-        # self.start_event_receiver()
+        self.start_event_receiver()
 
         self._component.set_op_callbacks(
             _update_device_callback,
