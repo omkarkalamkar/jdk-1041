e91bfaa830c59ca099e8e1630ad8adaaed3d954a
shraddhabajare <shraddhab.ska@gmail.com>
2022-07-26 12:36:53 +0530

    SAH-1149 - Update helper subarray class

--- tests/unit/mid/test_telescope_state.py
+++ tests/unit/mid/test_telescope_state.py
@@ -62,7 +62,7 @@ def set_one_device_fault(devFactory, cm, expected_elapsed_time):
 def test_telescope_state_fault_over_standby(tango_context):
     devFactory = DevFactory()
     cm = create_cm_no_faulty_devices(tango_context, True, True)
-    set_one_device_fault(devFactory, cm, 1.5)
+    set_one_device_fault(devFactory, cm, 12)
     assert cm.component.telescope_state == tango.DevState.FAULT
 
 
