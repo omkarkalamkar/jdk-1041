9480c4c1c71d69093a7c2b383e9aba720dd7cd4e
shraddhabajare <shraddhab.ska@gmail.com>
2022-08-02 18:09:52 +0530

    SAH-1168 - Fix formatting

--- src/ska_tmc_centralnode/manager/component_manager.py
+++ src/ska_tmc_centralnode/manager/component_manager.py
@@ -4,7 +4,6 @@ This module provided a reference implementation of a BaseComponentManager.
 It is provided for explanatory purposes, and to support testing of this
 package.
 """
-import json
 import time
 from typing import Callable, Optional
 
