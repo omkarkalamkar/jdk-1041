81fba38da504cbc762873249fd09973dab1c7775
Manthan Ambekar <manthan.ska@gmail.com>
2022-08-03 18:52:19 +0530

    SAH : Modify input validator test

--- tests/unit/mid/input_validator_test.py
+++ tests/unit/mid/input_validator_test.py
@@ -145,7 +145,7 @@ class TestAssignResourceValidator:
         "ska_mid/tm_leaf_node/d0004",
     ]
 
-    @pytest.mark.off(reason="New JSON changes to be updated")
+    # @pytest.mark.skip(reason="New JSON changes to be updated")
     def test_validate_good_json(self):
         """This function tests the validate method when good formatted json is provided"""
 
@@ -159,7 +159,7 @@ class TestAssignResourceValidator:
         )
         assert output_config == sample_assign_resources_request
 
-    @pytest.mark.off(reason="New JSON changes to be updated")
+    # @pytest.mark.skip(reason="New JSON changes to be updated")
     def test_validate_wrong_subarray_id(self):
         """
         Tests that InvalidJSONError is raised when a wrong subarray id is given
@@ -179,9 +179,9 @@ class TestAssignResourceValidator:
         with pytest.raises(SubarrayNotPresentError) as excinfo:
             input_validator.loads(json.dumps(input_json))
 
-    @pytest.mark.off(
-        reason="Behavior of this test case has changed in tox env."
-    )
+    # @pytest.mark.skip(
+    #     reason="Behavior of this test case has changed in tox env."
+    # )
     def test_validate_incorrect_receptor_id(self):
         """
         Tests that ResourceNotPresentError is raised when a receptor id is given incorrect
