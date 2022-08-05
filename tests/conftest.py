e463363201c8d0c22f0f4ac703a4454900c2870b
shraddhabajare <shraddhab.ska@gmail.com>
2022-08-05 16:23:58 +0530

    SAH-1156 - Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -60,7 +60,8 @@ def change_event_callbacks() -> MockTangoEventCallbackGroup:
         callbacks by name.
     """
     return MockTangoEventCallbackGroup(
+        "longRunningCommandsInQueue",
         "longRunningCommandResult",
-        "longRunningCommandsInQueue" "telescopeState",
+        "telescopeState",
         timeout=30.0,
     )
