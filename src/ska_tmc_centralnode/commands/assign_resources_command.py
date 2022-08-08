0298c5c522a7bb7f3b5d9ab045000b7e90002906
shraddhabajare <shraddhab.ska@gmail.com>
2022-08-08 15:06:14 +0530

    SAH-1158 - Address review comments

--- src/ska_tmc_centralnode/commands/assign_resources_command.py
+++ src/ska_tmc_centralnode/commands/assign_resources_command.py
@@ -194,10 +194,8 @@ class AssignResources(AbstractAssignReleaseResources):
         #     self.logger,
         # )
         # json_argument = input_validator.loads(argin)
-        self.logger.debug("Actual do_mid hook")
         try:
-            self.logger.debug(f"argin is:{argin}")
-            self.logger.debug("loading json input string")
+            self.logger.debug(f"Loading json string:{argin}")
             json_argument = json.loads(argin)
         except Exception as e:
             return self.generate_command_result(
@@ -231,7 +229,6 @@ class AssignResources(AbstractAssignReleaseResources):
                 "subarray_id key is not present in the input json argument.",
             )
 
-        self.logger.debug("Calling Init_adapters for do_mid")
         ret_code, message = self.init_adapters()
         if ret_code == ResultCode.FAILED:
             return ret_code, message
@@ -241,9 +238,6 @@ class AssignResources(AbstractAssignReleaseResources):
         ret_code, message = self.get_subarray_adapter(subarrayID)
         if ret_code == ResultCode.FAILED:
             return ret_code, message
-        self.logger.debug(
-            f"res_code after get_subarray_adapter::{ret_code} message after get_subarray_adapter::{message}"
-        )
 
         # check allocated dishes
         if "dish" not in json_argument:
@@ -261,21 +255,20 @@ class AssignResources(AbstractAssignReleaseResources):
         receptor_ids = json_argument["dish"]["receptor_ids"]
         self.logger.debug(f"receptor_ids are:{receptor_ids}")
         for receptor_id in receptor_ids:
-            self.logger.debug(f"receptor_id is:{receptor_id}")
             dish_ID = "dish" + receptor_id
             self.logger.debug(f"dish_ID is:{dish_ID}")
-            self.logger.debug(f"Type for dish_ID is:{type(dish_ID)}")
-            self.logger.debug(f"Type str dish_ID is:{str(dish_ID)}")
             if self.component_manager.is_already_assigned(dish_ID):
                 return self.generate_command_result(
                     ResultCode.FAILED,
                     ("Dish %s is already allocated", dish_ID),
                 )
             else:
-                self.logger.info("Resources are not assigned")
+                self.logger.info("Resources are already assigned")
 
         # is it necessary to make a copy? leave it as it was. MDC 29 Sept 2021
-        self.logger.debug("Invoking AssignResources command on TMC subarrays")
+        self.logger.debug(
+            f"Invoking AssignResources command on:{self.my_subarray_adapter}"
+        )
         ret_code, message = self.send_command(
             [self.my_subarray_adapter],
             "Error in calling AssignResources on subarray",
@@ -285,7 +278,7 @@ class AssignResources(AbstractAssignReleaseResources):
         if ret_code == ResultCode.FAILED:
             return ret_code, message
         self.logger.debug(
-            "AssignResources command on TMC Subarrays is Successful."
+            f"Resources assigned successfully to:{self.my_subarray_adapter}"
         )
         return (ResultCode.OK, "")
 
