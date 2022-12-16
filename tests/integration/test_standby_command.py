889765b2c77a4c47eec0ba79516045efca3bd47f
Roshan Tayde <roshant.ska@gmail.com>
2022-12-16 20:17:43 +0530

    him-109 central node mid low pipeline test with updated deployment charts.

--- tests/integration/test_standby_command.py
+++ tests/integration/test_standby_command.py
@@ -89,6 +89,7 @@ def test_standby_command_mid(tango_context, change_event_callbacks):
 
     assert central_node.telescopeState == DevState.STANDBY
 
+
 @pytest.mark.skip(reason="Test case is failing")
 @pytest.mark.post_deployment
 @pytest.mark.SKA_low
