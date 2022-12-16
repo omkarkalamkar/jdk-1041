5e55a0705dab0bb1dfb3cfebec06d7fb540a090f
yogeshk.ska <yogeshk.ska@gmail.com>
2022-12-16 17:48:24 +0530

    HM-111: Lint issue solve

--- tests/unit/commands/low/test_assign_resources_command.py
+++ tests/unit/commands/low/test_assign_resources_command.py
@@ -10,9 +10,7 @@ from ska_tmc_common.exceptions import CommandNotAllowed
 from ska_tmc_common.test_helpers.helper_adapter_factory import (
     HelperAdapterFactory,
 )
-from ska_tmc_common.test_helpers.helper_state_mccsdevice import (
-    HelperMCCSStateDevice,
-)
+
 from tango import DevState
 
 from ska_tmc_centralnode.commands.assign_resources_command import (
