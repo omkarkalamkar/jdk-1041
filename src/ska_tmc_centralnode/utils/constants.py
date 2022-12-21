dfe78e954b0fe6205990a07372ce6ce1cf42ff7b
yogeshk.ska <yogeshk.ska@gmail.com>
2022-12-21 11:53:13 +0530

    HM-111: Lint issue solved

--- src/ska_tmc_centralnode/utils/constants.py
+++ src/ska_tmc_centralnode/utils/constants.py
@@ -1,15 +1,6 @@
 """Common constant used in centralnode
 """
 
-REQUIRED_LOW_ASSIGN_RESOURCE_KEYS = [
-    "subarray_id",
-    "sdp",
-    "csp",
-    "mccs"
-]
+REQUIRED_LOW_ASSIGN_RESOURCE_KEYS = ["subarray_id", "sdp", "csp", "mccs"]
 
-MCCS_REQUIRED_KEYS = [
-    "subarray_beam_ids",
-    "station_ids",
-    "channel_blocks"
-]
+MCCS_REQUIRED_KEYS = ["subarray_beam_ids", "station_ids", "channel_blocks"]
