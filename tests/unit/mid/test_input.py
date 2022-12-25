f0ae093c79b703547d85fcda406baab67964d46d
Bhushan Parakh <bhushan.ska@gmail.com>
2022-12-25 19:37:33 +0530

    HM-110: Resolved review comments.

--- tests/unit/mid/test_input.py
+++ tests/unit/mid/test_input.py
@@ -3,12 +3,12 @@ from ska_tmc_centralnode.model.input import InputParameterMid
 
 def test_properties():
     input = InputParameterMid(None)
-    input.tm_subarray_dev_names = ("1", "2")
-    assert input.tm_subarray_dev_names == ("1", "2")
+    input.subarray_dev_names = ("1", "2")
+    assert input.subarray_dev_names == ("1", "2")
     input.csp_subarray_dev_names = ("3", "4")
     assert input.csp_subarray_dev_names == ("3", "4")
-    input.tm_dish_dev_names = "5"
-    assert input.tm_dish_dev_names == ("5")
+    input.dish_leaf_node_dev_names = "5"
+    assert input.dish_leaf_node_dev_names == ("5")
     input.dish_dev_names = "5"
     assert input.dish_dev_names == ("5")
     input.sdp_subarray_dev_names = "6"
@@ -17,7 +17,7 @@ def test_properties():
     assert input.csp_master_dev_name == "7"
     input.sdp_master_dev_name = "8"
     assert input.sdp_master_dev_name == "8"
-    input.tm_leaf_sdp_master_dev_name = "9"
-    assert input.tm_leaf_sdp_master_dev_name == "9"
-    input.tm_leaf_csp_master_dev_name = "10"
-    assert input.tm_leaf_csp_master_dev_name == "10"
+    input.sdp_mln_dev_name = "9"
+    assert input.sdp_mln_dev_name == "9"
+    input.csp_mln_dev_name = "10"
+    assert input.csp_mln_dev_name == "10"
