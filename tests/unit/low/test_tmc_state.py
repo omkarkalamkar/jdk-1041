45a30be38a6710e78d34f6fd18ae4087ec0fd9d6
muskansharma <muskans.ska@gmail.com>
2023-01-16 10:48:59 +0530

    HM-146:Resolved review comment

--- tests/unit/low/test_tmc_state.py
+++ tests/unit/low/test_tmc_state.py
@@ -42,7 +42,6 @@ def set_device_init(devFactory, cm, expected_elapsed_time):
     ensure_tmc_op_state(cm, tango.DevState.INIT, expected_elapsed_time)
 
 
-@pytest.mark.MST
 @pytest.mark.SKA_low
 def test_tmc_state_init(tango_context):
     # import debugpy; debugpy.debug_this_thread()
