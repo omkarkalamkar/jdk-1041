03a78875cb015aa3178e105b0cf0e6b95b9a4525
user <jayant.ska@gmail.com>
2023-02-13 15:38:23 +0530

    SAH-1283: Formatting

--- src/ska_tmc_centralnode/input_validator.py
+++ src/ska_tmc_centralnode/input_validator.py
@@ -110,7 +110,9 @@ class AssignResourceValidator:
         # Check if JSON is correct
         self.logger.info("Checking JSON format.")
         try:
-            assign_request = CODEC.loads(AssignResourcesRequestSchema, input_string)
+            assign_request = CODEC.loads(
+                AssignResourcesRequestSchema, input_string
+            )
         except (ValidationError, JSONDecodeError) as json_error:
             self.logger.exception("Exception: %s", str(json_error))
             exception_message = (
