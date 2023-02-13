ee7b7e462f38b803227cabffe25a25fea2d25f50
user <jayant.ska@gmail.com>
2023-02-13 15:35:52 +0530

    SAH-1283: Update cdm api

--- src/ska_tmc_centralnode/input_validator.py
+++ src/ska_tmc_centralnode/input_validator.py
@@ -14,7 +14,7 @@ from json import JSONDecodeError
 
 from marshmallow import ValidationError
 from ska_tmc_cdm.messages.central_node.assign_resources import (
-    AssignResourcesRequest,
+    AssignResourcesRequestSchema,
 )
 
 # SKA specific imports
@@ -110,7 +110,7 @@ class AssignResourceValidator:
         # Check if JSON is correct
         self.logger.info("Checking JSON format.")
         try:
-            assign_request = CODEC.loads(AssignResourcesRequest, input_string)
+            assign_request = CODEC.loads(AssignResourcesRequestSchema, input_string)
         except (ValidationError, JSONDecodeError) as json_error:
             self.logger.exception("Exception: %s", str(json_error))
             exception_message = (
