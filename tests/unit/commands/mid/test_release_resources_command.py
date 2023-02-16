46493a5a913db7be060f831ff1c68b3720a44ac7
devesh pandey <devesh.ska@gmail.com>
2023-02-16 19:18:44 +0530

    HM-165:fix lint issues

--- tests/unit/commands/mid/test_release_resources_command.py
+++ tests/unit/commands/mid/test_release_resources_command.py
@@ -140,7 +140,6 @@ def test_mid_release_resources_command_missing_subarray_id(
     assert "subarray_id" in message
 
 
-
 def test_telescope_release_resources_fail_check_allowed(tango_context):
     cm, start_time = create_cm()
     elapsed_time = time.time() - start_time
