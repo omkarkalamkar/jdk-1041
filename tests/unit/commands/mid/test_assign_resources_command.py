ac781fe7f45a92d7741acf0b44d4e1d853a59267
mansi.shirore <mansis.ska@gmail.com>
2023-02-21 17:41:00 +0530

    SAH-1283: Updated unit test cases.

--- tests/unit/commands/mid/test_assign_resources_command.py
+++ tests/unit/commands/mid/test_assign_resources_command.py
@@ -171,8 +171,9 @@ def test_telescope_assign_resources_command_empty_input_json(
     logger.info("%s", tango_context)
     assign_res_command, _, cm = get_assign_resources_command_obj()
     cm.is_command_allowed("AssignResources")
-    with pytest.raises(InvalidJSONError):
-        cm.assign_resources(" ", task_callback=task_callback)
+    cm.assign_resources("", task_callback=task_callback)
+    (res_code, _) = assign_res_command.do("")
+    assert res_code == ResultCode.FAILED
 
 
 def test_assign_resources_command_missing_subarray_id(
