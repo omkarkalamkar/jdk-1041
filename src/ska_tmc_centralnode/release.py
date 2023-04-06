f01f869e097c55b6b537e81cf7eb096de37d9211
Bhushan Parakh <bhushan.ska@gmail.com>
2023-04-06 18:14:54 +0530

    HM-195: Resolve comment

--- src/ska_tmc_centralnode/release.py
+++ src/ska_tmc_centralnode/release.py
@@ -12,9 +12,9 @@
 name = """ska-tmc-centralnode"""
 version = "0.7.1"
 version_info = version.split(".")
-description = """Using latest common package with updated helper devices."""
+description = """Central Node is a coordinator of the complete M&C system."""
 author = "Team NCRA"
 author_email = "telmgt-internal@googlegroups.com"
 license = """BSD-3-Clause"""
-url = """https://www.skaobservatory.org/"""
+url = """https://www.skao.int/"""
 copyright = """"""
