50b7dcdcdc7035fa96f1d4ea29f6346bc8bc27ad
Muskan Sharma <muskans.ska@gmail.com>
2023-04-21 16:02:00 +0530

    HM-184: Resolved Review Comments.

--- tests/unit/mid/test_telescope_state_off.py
+++ tests/unit/mid/test_telescope_state_off.py
@@ -52,7 +52,6 @@ def devices_to_load():
     )
 
 
-@pytest.mark.OFF
 def test_telescope_state_off(tango_context):
     cm = create_cm_no_faulty_devices(tango_context, True, True)
     set_devices_state(
--- tests/unit/mid/test_telescope_state_on.py
+++ tests/unit/mid/test_telescope_state_on.py
@@ -52,7 +52,6 @@ def devices_to_load():
     )
 
 
-@pytest.mark.ON
 def test_telescope_state_on(tango_context):
     cm = create_cm_no_faulty_devices(tango_context, True, True)
     set_devices_state(
