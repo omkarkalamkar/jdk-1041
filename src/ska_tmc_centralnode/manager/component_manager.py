23164185317dac8a54a92f5bdd50d381b354fb37
JayantKumbhar <jayant.ska@gmail.com>
2023-05-04 19:03:48 +0530

    SAH-1320: Resolve review comments

--- src/ska_tmc_centralnode/manager/component_manager.py
+++ src/ska_tmc_centralnode/manager/component_manager.py
@@ -15,7 +15,7 @@ from ska_tmc_common.device_info import (
     SubArrayDeviceInfo,
 )
 from ska_tmc_common.enum import LivelinessProbeType
-from ska_tmc_common.exceptions import CommandNotAllowed
+from ska_tmc_common.exceptions import CommandNotAllowed, InvalidJSONError
 from ska_tmc_common.tmc_component_manager import TmcComponentManager
 from tango import DevState
 
@@ -547,7 +547,7 @@ class CNComponentManager(TmcComponentManager):
                 )
 
                 json_argument = assign_validator.loads(json.dumps(argin))
-            except Exception as e:
+            except InvalidJSONError as e:
                 return assign_resources_command.reject_command(str(e))
 
         # validate processing block
@@ -610,7 +610,7 @@ class CNComponentManager(TmcComponentManager):
             try:
                 release_validator = ReleaseResourceValidator(self.logger)
                 json_argument = release_validator.loads(json.dumps(argin))
-            except Exception as e:
+            except InvalidJSONError as e:
                 return release_resources_command.reject_command(str(e))
 
         task_status, response = self.submit_task(
