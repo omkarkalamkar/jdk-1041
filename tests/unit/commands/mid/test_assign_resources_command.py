33cca8359cf1a9e2bcad1f8389a8270a74567755
roshant.ska <roshant.ska@gmail.com>
2023-06-30 16:23:17 +0530

    HM-258: Fixing issus in Component Manager.

--- tests/unit/commands/mid/test_assign_resources_command.py
+++ tests/unit/commands/mid/test_assign_resources_command.py
@@ -98,6 +98,7 @@ def get_assign_resources_command_obj():
     )
     return assign_res_command, adapter_factory, cm
 
+
 def test_assign_resources_command_completed(tango_context, task_callback):
     logger.info("%s", tango_context)
     cm, start_time = create_cm()
