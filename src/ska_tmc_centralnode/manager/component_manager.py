4ff56b384dd5c4315d8cb224eb9decb895234afe
roshant.ska <roshant.ska@gmail.com>
2023-07-03 12:48:05 +0530

    HM-258: Resolve review comments.

--- src/ska_tmc_centralnode/manager/component_manager.py
+++ src/ska_tmc_centralnode/manager/component_manager.py
@@ -582,7 +582,14 @@ class CNComponentManager(TmcComponentManager):
         """
         Submit the AssignResources command in queue.
 
-        :return: a result code and message
+        Description:
+        Submits the AssignResources command to the queue for processing.
+
+        Arguments:
+        - json_argument (str): A dictionary containing the necessary information for assigning resources.
+
+        Returns:
+        - None: No return value.
         """
 
         # Execute the command if the input JSON is valid
@@ -638,7 +645,6 @@ class CNComponentManager(TmcComponentManager):
                 return assign_resources_command.reject_command(str(e))
 
         # Reject command if Subarray is not available
-        json_argument = json.loads(argin)
         subarray_id = json_argument["subarray_id"]
         subarray_suffics = "/" + str(subarray_id)
         subarrays_list = list(
