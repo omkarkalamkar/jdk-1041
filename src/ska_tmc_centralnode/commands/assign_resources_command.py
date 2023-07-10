144da59d2af72779d79c982dcf274e53a449da5a
akshaysonawane <akshayss.ska@gmail.com>
2023-07-10 11:54:54 +0530

    HM-248: resolve review comment

--- src/ska_tmc_centralnode/commands/assign_resources_command.py
+++ src/ska_tmc_centralnode/commands/assign_resources_command.py
@@ -42,7 +42,7 @@ class AssignResources(AbstractAssignReleaseResources):
         )
         self.dish_adapters = []
         self.subarray_adapters = []
-        self.tm_subarray_adapter: SubArrayAdapter
+        self.tm_subarray_adapter: SubArrayAdapter = None
         self._skuid = skuid
         self.task_callback: Callable
 
