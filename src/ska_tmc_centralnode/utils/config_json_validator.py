bafdaa417c8d2a916dcdc6af2b18a266f1e5c7fe
yogeshk.ska <yogeshk.ska@gmail.com>
2023-10-16 19:36:10 +0530

    HM-343: dish config validator added

--- src/ska_tmc_centralnode/utils/config_json_validator.py
+++ src/ska_tmc_centralnode/utils/config_json_validator.py
@@ -1,6 +1,6 @@
 class DishConfigValidator:
-    """This class implement method to validate DishConfig json
-    """
+    """This class implement method to validate DishConfig json"""
+
     def __init__(self, dish_config_json: dict):
         """
         params:
@@ -9,39 +9,38 @@ class DishConfigValidator:
         self.dish_config_json = dish_config_json
 
     def _get_vcc_k_values(self):
-        """Extract vcc and k values from dish config
-        """
+        """Extract vcc and k values from dish config"""
         vcc_ids, k_values = [], []
-        for _, vcc_k_map in self.dish_config_json['dish_parameters'].items():
+        for _, vcc_k_map in self.dish_config_json["dish_parameters"].items():
             vcc_id = vcc_k_map.get("vcc")
             k_value = vcc_k_map.get("k")
             vcc_ids.append(vcc_id)
             k_values.append(k_value)
         return vcc_ids, k_values
-    
+
     def _is_valid_k_values(self, k_values: list):
         """
         params:
         k_values(list): List of k values to validate
         """
-        k_value_range = range(1,2223)
+        k_value_range = range(1, 2223)
         return all(k_value in k_value_range for k_value in k_values)
-    
+
     def _is_valid_vcc_ids(self, vcc_ids: list):
         """
         params:
         vcc_ids(list): List of VCC ids
         """
         return len(vcc_ids) == len(set(vcc_ids))
-    
+
     def _is_valid_dish_ids(self, dish_id_list):
-        """Validate Dish Ids are unique and validate 
+        """Validate Dish Ids are unique and validate
         Dish Id are within valid range
         """
         # Check if values are unique
         if len(dish_id_list) != len(set(dish_id_list)):
             return False
-        
+
         # Check if dish id are within valid range
         for dish_id in dish_id_list:
             if dish_id.startswith("SKA"):
@@ -55,7 +54,6 @@ class DishConfigValidator:
             else:
                 return False
         return True
-
 
     def is_json_valid(self):
         """This methid validate json as per following rules
@@ -89,7 +87,7 @@ class DishConfigValidator:
         vcc_ids, k_values = self._get_vcc_k_values()
         if (
             not self._is_valid_dish_ids(dish_ids)
-            or not self._is_valid_vcc_ids(vcc_ids) 
+            or not self._is_valid_vcc_ids(vcc_ids)
             or not self._is_valid_k_values(k_values)
         ):
             return False
--- tests/unit/mid/test_dish_config_json_validator.py
+++ tests/unit/mid/test_dish_config_json_validator.py
@@ -2,115 +2,65 @@ import pytest
 
 from ska_tmc_centralnode.utils.config_json_validator import DishConfigValidator
 
+
 @pytest.mark.validate
 class TestDishConfigValidator:
     """Implement test cases to validate Dish Config
     Json
     """
+
     def test_valid_dish_config_json(self):
-        """Validate Correct Dish Config json
-        """
+        """Validate Correct Dish Config json"""
         dish_config_json = {
             "interface": "https://schema.skao.int/ska-mid-cbf-initial-parameters/2.2",
             "dish_parameters": {
-                "SKA001": {
-                    "vcc": 1,
-                    "k"  : 11
-                },
-                "SKA100": {
-                    "vcc": 2,
-                    "k"  : 101
-                },
-                "SKA036": {
-                    "vcc": 3,
-                    "k"  : 1127
-                },
-                "SKA133": {
-                    "vcc": 4,
-                    "k"  : 620
-                }
-            }
+                "SKA001": {"vcc": 1, "k": 11},
+                "SKA100": {"vcc": 2, "k": 101},
+                "SKA036": {"vcc": 3, "k": 1127},
+                "SKA133": {"vcc": 4, "k": 620},
+            },
         }
         dish_config_validator = DishConfigValidator(dish_config_json)
-        assert dish_config_validator.is_json_valid() == True
+        assert dish_config_validator.is_json_valid() is True
 
     def test_dish_config_validator_for_invalid_dishids(self):
-        """Validate Dish Config Json validator when dish ids are invalid
-        """
+        """Validate Dish Config Json validator when dish ids are invalid"""
         dish_config_json = {
             "interface": "https://schema.skao.int/ska-mid-cbf-initial-parameters/2.2",
             "dish_parameters": {
-                "ABC001": {
-                    "vcc": 1,
-                    "k"  : 11
-                },
-                "SKA100": {
-                    "vcc": 2,
-                    "k"  : 101
-                },
-                "SKA036": {
-                    "vcc": 3,
-                    "k"  : 1127
-                },
-                "SKA133": {
-                    "vcc": 4,
-                    "k"  : 620
-                }
-            }
+                "ABC001": {"vcc": 1, "k": 11},
+                "SKA100": {"vcc": 2, "k": 101},
+                "SKA036": {"vcc": 3, "k": 1127},
+                "SKA133": {"vcc": 4, "k": 620},
+            },
         }
         dish_config_validator = DishConfigValidator(dish_config_json)
-        assert dish_config_validator.is_json_valid() == False
-    
+        assert dish_config_validator.is_json_valid() is False
+
     def test_dish_config_validator_for_invalid_dishids_range(self):
-        """Validate Dish Config Json validator when dish ids are not within range
-        """
+        """Validate Dish Config Json validator when dish ids are not within range"""
         dish_config_json = {
             "interface": "https://schema.skao.int/ska-mid-cbf-initial-parameters/2.2",
             "dish_parameters": {
-                "SKA187": {
-                    "vcc": 1,
-                    "k"  : 11
-                },
-                "SKA100": {
-                    "vcc": 2,
-                    "k"  : 101
-                },
-                "SKA036": {
-                    "vcc": 3,
-                    "k"  : 1127
-                },
-                "SKA133": {
-                    "vcc": 4,
-                    "k"  : 620
-                }
-            }
+                "SKA187": {"vcc": 1, "k": 11},
+                "SKA100": {"vcc": 2, "k": 101},
+                "SKA036": {"vcc": 3, "k": 1127},
+                "SKA133": {"vcc": 4, "k": 620},
+            },
         }
         dish_config_validator = DishConfigValidator(dish_config_json)
-        assert dish_config_validator.is_json_valid() == False
+        assert dish_config_validator.is_json_valid() is False
 
     def test_dish_config_validator_for_duplicate_vcc_ids(self):
-        """Validate Dish Config Json validator when vcc ids are not unique
-        """
+        """Validate Dish Config Json validator when vcc ids are not unique"""
         dish_config_json = {
             "interface": "https://schema.skao.int/ska-mid-cbf-initial-parameters/2.2",
             "dish_parameters": {
-                "SKA001": {
-                    "vcc": 1,
-                    "k"  : 11
-                },
-                "SKA100": {
-                    "vcc": 2,
-                    "k"  : 101
-                },
-                "SKA036": {
-                    "vcc": 3,
-                    "k"  : 1127
-                },
-                "SKA101": {
-                    "vcc": 1,
-                    "k"  : 620
-                }
-            }
+                "SKA001": {"vcc": 1, "k": 11},
+                "SKA100": {"vcc": 2, "k": 101},
+                "SKA036": {"vcc": 3, "k": 1127},
+                "SKA101": {"vcc": 1, "k": 620},
+            },
         }
         dish_config_validator = DishConfigValidator(dish_config_json)
-        assert dish_config_validator.is_json_valid() == False
+        assert dish_config_validator.is_json_valid() is False
