50c7e30df8298e6909f1f178d3217ef2d40c8975
akshaysonawane <akshayss.ska@gmail.com>
2023-10-23 12:41:53 +0530

    SKB-257: rectify the mistake

--- src/ska_tmc_centralnode/manager/event_receiver.py
+++ src/ska_tmc_centralnode/manager/event_receiver.py
@@ -62,7 +62,9 @@ class CentralNodeEventReceiver(EventReceiver):
                     self._component_manager.input_parameter, InputParameterMid
                 ):
                     if (
-                        self._component_manager.input_parameter.dish_dev_names
+                        self._component_manager.input_parameter.dish_dev_names[
+                            0
+                        ]
                         in dev_info.dev_name
                     ):
                         proxy.subscribe_event(
