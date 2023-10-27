bae01176cb1cea373f032b85dff84b4c38ad55fe
devesh.ska <devesh.ska@gmail.com>
2023-10-27 15:49:55 +0530

    HM-332:Fix reset placeholder method

---
+++ tests/integration/test_reset_command.py
@@ -0,0 +1,23 @@
+import pytest
+from ska_tango_base.commands import ResultCode
+from ska_tmc_common.dev_factory import DevFactory
+
+
+def reset_command(central_node_fqdn):
+    dev_factory = DevFactory()
+    central_node = dev_factory.get_device(central_node_fqdn)
+    result_code, message = central_node.Reset()
+    assert result_code == ResultCode.REJECTED
+    assert message == "Reset command is not implemented"
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_mid
+def test_reset_command_mid():
+    reset_command("ska_mid/tm_central/central_node")
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_low
+def test_reset_command_low():
+    reset_command("ska_mid/tm_central/central_node")
