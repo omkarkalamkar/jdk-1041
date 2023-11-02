f34024d0147b48ae11281d091eb3097383ea4884
SKAIN <truptin.ska@gmail.com>
2023-11-02 15:36:29 +0530

    SAH-1415 :  lint update

--- src/ska_tmc_centralnode/manager/component_manager.py
+++ src/ska_tmc_centralnode/manager/component_manager.py
@@ -2,6 +2,7 @@
 This module provided an implementation of the Central Node ComponentManager.
 """
 from __future__ import annotations
+
 import json
 import time
 from typing import Callable, Optional
