8d4cc9d9be1b14c34e4022623200b6ee4b1acda6
Mansi Shirore <mansis.ska@gmail.com>
2023-11-06 17:13:18 +0530

    SAH-1415: Updated Component manger.

--- src/ska_tmc_centralnode/commands/load_dish_config_command.py
+++ src/ska_tmc_centralnode/commands/load_dish_config_command.py
@@ -51,6 +51,7 @@ class LoadDishCfg(LoadDishCfgCommand):
         """
         # Indicate that the task has started
         self.task_callback = task_callback
+        self.set_command_id(__class__.__name__)
         self.task_callback(status=TaskStatus.IN_PROGRESS)
         self.component_manager.command_in_progress = "LoadDishCfg"
         self.component_manager.command_result = ResultCode.STARTED
