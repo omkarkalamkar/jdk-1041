7077eb06d4a5e852056feae82ec143b5600b29c0
manthan <manthan.ska@gmail.com>
2023-11-07 16:33:20 +0530

    SAH-1431: Updated unit test cases

--- tests/unit/commands/low/test_off_command.py
+++ tests/unit/commands/low/test_off_command.py
@@ -11,10 +11,13 @@ from ska_tmc_common.test_helpers.helper_adapter_factory import (
 from tango import DevState
 
 from ska_tmc_centralnode.model.input import InputParameterLow
+from tests.mock_callable import MockCallable
 from tests.settings import (
     LOW_CSP_MLN_DEVICE,
     LOW_SDP_MLN_DEVICE,
+    MCCS_MLN_DEVICE,
     check_cspmln_availability,
+    check_mccsmln_availability,
     check_sdpmln_availability,
     create_cm,
     logger,
@@ -54,6 +57,41 @@ def test_low_telescope_off_command(tango_context, task_callback):
     task_callback.assert_against_call(
         call_kwargs={"status": TaskStatus.COMPLETED, "result": ResultCode.OK}
     )
+
+
+@pytest.mark.SKA_low
+def test_telescope_off_command_unavailability(tango_context):
+    logger.info("%s", tango_context)
+    # import debugpy; debugpy.debug_this_thread()
+    cm, start_time = create_cm()
+    elapsed_time = time.time() - start_time
+    logger.info(
+        "checked %s devices in %s", len(cm.checked_devices), elapsed_time
+    )
+    unique_id = f"{time.time()}_TelescopeOff"
+    task_callback = MockCallable(unique_id)
+    dev_factory = DevFactory()
+    csp_mln = dev_factory.get_device(LOW_CSP_MLN_DEVICE)
+    sdp_mln = dev_factory.get_device(LOW_CSP_MLN_DEVICE)
+    mccs_mln = dev_factory.get_device(MCCS_MLN_DEVICE)
+    csp_mln.SetisSubsystemAvailable(False)
+    sdp_mln.SetisSubsystemAvailable(False)
+    mccs_mln.SetisSubsystemAvailable(False)
+    check_cspmln_availability(cm, False)
+    check_sdpmln_availability(cm, False)
+    check_mccsmln_availability(cm, False)
+    assert (cm.component.telescope_availability)[
+        "csp_master_leaf_node"
+    ] is False
+    assert (cm.component.telescope_availability)[
+        "sdp_master_leaf_node"
+    ] is False
+    assert (cm.component.telescope_availability)["_master_leaf_node"] is False
+    cm.is_command_allowed("TelescopeOn")
+
+    cm.telescope_on(task_callback=task_callback)
+    time.sleep(1)
+    assert task_callback.result == ResultCode.OK
 
 
 @pytest.mark.SKA_low
