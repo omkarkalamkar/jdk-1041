963a469f656b117c35991b374ff133b59c31c46b
akshaysonawane <akshayss.ska@gmail.com>
2023-12-19 09:44:33 +0530

    HM-374: Resolve review comments

--- src/ska_tmc_centralnode/central_node_low.py
+++ src/ska_tmc_centralnode/central_node_low.py
@@ -38,15 +38,15 @@ class CentralNodeLow(AbstractCentralNode):
 
     SdpMasterFQDN = device_property(dtype="str")
 
-    TMCMidCspSubarrayLeafNodes = device_property(
+    TMCLowCspSubarrayLeafNodes = device_property(
         dtype=("str",),
-        doc="List of TM Mid CspSubarrayLeafNode devices",
+        doc="List of TM Low CspSubarrayLeafNode devices",
         default_value=tuple(),
     )
 
-    TMCMidSdpSubarrayLeafNodes = device_property(
+    TMCLowSdpSubarrayLeafNodes = device_property(
         dtype=("str",),
-        doc="List of TM Mid SdpSubarrayLeafNode devices",
+        doc="List of TM Low SdpSubarrayLeafNode devices",
         default_value=tuple(),
     )
 
@@ -193,10 +193,10 @@ class CentralNodeLow(AbstractCentralNode):
         cm.input_parameter.csp_master_dev_name = self.CspMasterFQDN or ""
         cm.input_parameter.csp_mln_dev_name = self.CspMasterLeafNodeFQDN or ""
         cm.input_parameter.csp_subarray_dev_names = (
-            self.TMCMidCspSubarrayLeafNodes
+            self.TMCLowCspSubarrayLeafNodes
         )
         cm.input_parameter.sdp_subarray_dev_names = (
-            self.TMCMidSdpSubarrayLeafNodes
+            self.TMCLowSdpSubarrayLeafNodes
         )
         cm.update_input_parameter()
         return cm
