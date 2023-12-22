f3a4fb401026463c482325ae8acee85f293800be
apurvapatkar <apurva.ska@gmail.com>
2023-12-22 13:18:20 +0530

    SAH-1456: Execute tests.

--- tests/integration/test_load_dish_cfg.py
+++ tests/integration/test_load_dish_cfg.py
@@ -57,7 +57,7 @@ def load_dish_cfg(
 
     assert unique_id[0].endswith("LoadDishCfg")
     assert result[0] == ResultCode.QUEUED
-test_load_dish_cfg_when_csp_is_defective
+
     change_event_callbacks.assert_change_event(
         "longRunningCommandResult",
         (unique_id[0], str(int(ResultCode.OK))),
