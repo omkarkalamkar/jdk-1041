4ec1be6db89d255bf4d454c422d7909cfa42d775
apurvapatkar <apurva.ska@gmail.com>
2024-01-05 17:27:44 +0530

    SAH-1456: Fix unit tests.

--- tests/unit/mid/test_telescope_state.py
+++ tests/unit/mid/test_telescope_state.py
@@ -3,8 +3,8 @@ import tango
 from ska_tmc_common import HelperBaseDevice
 from ska_tmc_common.dev_factory import DevFactory
 from ska_tmc_common.enum import DishMode
+from ska_tmc_common.test_helpers.helper_dish_device import HelperDishDevice
 from ska_tmc_common.test_helpers.helper_subarray_device import (
-    HelperDishDevice,
     HelperSubArrayDevice,
 )
 
