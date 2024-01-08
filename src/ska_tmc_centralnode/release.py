a0b9752a348f276fe7c81f4b0a79a33686b9b0c5
yogeshk.ska <yogeshk.ska@gmail.com>
2024-01-08 19:06:44 +0530

    HM-387: relase updated

--- src/ska_tmc_centralnode/release.py
+++ src/ska_tmc_centralnode/release.py
@@ -10,7 +10,7 @@
 """Release information for Python Package"""
 
 name = """ska-tmc-centralnode"""
-version = "0.12.2"
+version = "0.13.0"
 version_info = version.split(".")
 description = """Central Node is a coordinator of the complete M&C system."""
 author = "Team Sahyadri, Team Himalay"
