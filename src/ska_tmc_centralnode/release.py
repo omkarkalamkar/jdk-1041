db78ab4493209ed906f6c35bc47ff1b70e66a4bd
Mansi Shirore <mansis.ska@gmail.com>
2024-01-09 15:03:06 +0530

    SAH-1471: Updated release version.

--- src/ska_tmc_centralnode/release.py
+++ src/ska_tmc_centralnode/release.py
@@ -10,7 +10,7 @@
 """Release information for Python Package"""
 
 name = """ska-tmc-centralnode"""
-version = "0.12.0"
+version = "0.12.3"
 version_info = version.split(".")
 description = """Central Node is a coordinator of the complete M&C system."""
 author = "Team Sahyadri, Team Himalay"
