42fc70d2c03ca5b692c98f0cd302dfcf12aa20e5
Mansi Shirore <mansis.ska@gmail.com>
2024-01-11 21:15:47 +0530

    SAH-1471: Updated component manager.

--- src/ska_tmc_centralnode/manager/component_manager.py
+++ src/ska_tmc_centralnode/manager/component_manager.py
@@ -865,7 +865,27 @@ class CNComponentManager(TmcComponentManager):
                     )
         return True
 
-    def check_subarray_id_in_json(self, json_argument):
+    def is_input_json_is_valid(self, argin: str):
+        """
+        Checks inputs json.
+
+        param argin: input json string
+        :type argin: str
+        """
+        try:
+            json_argument = json.loads(argin)
+            self.logger.debug("JSON argin is in correct format.")
+            return True, json_argument
+        except json.JSONDecodeError as e:
+            return False, f"Problem in loading the JSON string: {e}"
+
+    def check_subarray_id_in_json(self, json_argument: str):
+        """
+        Checks subarray id is present in json or not.
+
+        param json_argument: input json string
+        :type json_argument: str
+        """
         try:
             subarray_id = json_argument["subarray_id"]
             return True, subarray_id
@@ -874,14 +894,6 @@ class CNComponentManager(TmcComponentManager):
                 False,
                 f"subarray_id key is not present in the input json argument: str{e}",
             )
-
-    def check_input_json_is_valid(self, argin):
-        try:
-            json_argument = json.loads(argin)
-            self.logger.debug("JSON argin is in correct format.")
-            return True, json_argument
-        except json.JSONDecodeError as e:
-            return False, f"Problem in loading the JSON string: {e}"
 
     def assign_resources(
         self, argin: str, task_callback: Optional[Callable] = None
@@ -897,7 +909,7 @@ class CNComponentManager(TmcComponentManager):
         :rtype: tuple
         """
 
-        is_json_valid, input_json_or_message = self.check_input_json_is_valid(
+        is_json_valid, input_json_or_message = self.is_input_json_is_valid(
             argin
         )
         if not is_json_valid:
@@ -1014,7 +1026,7 @@ class CNComponentManager(TmcComponentManager):
         :return: task_status
         :rtype: tuple
         """
-        is_json_valid, input_json_or_message = self.check_input_json_is_valid(
+        is_json_valid, input_json_or_message = self.is_input_json_is_valid(
             argin
         )
         if not is_json_valid:
@@ -1034,13 +1046,6 @@ class CNComponentManager(TmcComponentManager):
         release_resources_command = ReleaseResources(
             self, adapter_factory=self.adapter_factory, logger=self.logger
         )
-        try:
-            json_argument = json.loads(argin)
-            self.logger.debug("JSON argin is in correct format.")
-        except json.JSONDecodeError as e:
-            return release_resources_command.reject_command(
-                f"The JSON string is malformed. Error: {str(e)}"
-            )
 
         # Execute the command if the input JSON is valid
         if isinstance(self.input_parameter, InputParameterLow):
@@ -1048,7 +1053,7 @@ class CNComponentManager(TmcComponentManager):
                 is_valid,
                 invalid_json_error_msg,
             ) = release_resources_command._validate_low_json(
-                json_argument,
+                input_json_or_message,
                 REQUIRED_LOW_RELEASE_RESOURCE_KEYS,
             )
             if not is_valid:
@@ -1056,15 +1061,15 @@ class CNComponentManager(TmcComponentManager):
                     invalid_json_error_msg
                 )
         elif isinstance(self.input_parameter, InputParameterMid):
-            self.logger.info(f"Json argument::{json_argument}")
+            self.logger.info(f"Json argument::{input_json_or_message}")
             # Utilize CDM to validate json.
             try:
                 release_validator = ReleaseResourceValidator(self.logger)
-                json_argument = release_validator.loads(argin)
+                input_json_or_message = release_validator.loads(argin)
             except InvalidJSONError as e:
                 return release_resources_command.reject_command(str(e))
         # Reject command if Subarray is not available
-        subarray_id = json_argument["subarray_id"]
+        subarray_id = subarray_id_or_message
         subarray_suffics = "/" + str(subarray_id)
         subarrays_list = list(
             self._component.telescope_availability["tmc_subarrays"].keys()
@@ -1081,7 +1086,7 @@ class CNComponentManager(TmcComponentManager):
 
         task_status, response = self.submit_task(
             release_resources_command.release_resources,
-            args=[json_argument, self.logger],
+            args=[input_json_or_message, self.logger],
             task_callback=task_callback,
         )
         return task_status, response
