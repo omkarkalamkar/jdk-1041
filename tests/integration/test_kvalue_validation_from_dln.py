52416be3f856682722bcf2d03e8457729c1720b4
Roshan Tayde <roshant.ska@gmail.com>
2024-01-30 22:38:42 +0530

    HM-411: Fix test case issues.

--- tests/integration/test_kvalue_validation_from_dln.py
+++ tests/integration/test_kvalue_validation_from_dln.py
@@ -31,7 +31,10 @@ def test_dln_kvalue_validation_result(tango_context):
         "d0001": "k-value not identical",
     }
     assert wait_and_validate_device_attribute_value(
-        central_node, "DishVccValidationStatus", json.dumps(dict_to_compare), is_json=True,
+        central_node,
+        "DishVccValidationStatus",
+        json.dumps(dict_to_compare),
+        is_json=True,
     ), "Timeout while waiting for validating attribute value"
 
     # Verify if all dish leaf node gives k-value validation result as ResultCode.Ok
