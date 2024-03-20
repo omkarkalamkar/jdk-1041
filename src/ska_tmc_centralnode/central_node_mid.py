2290d4022d836272f474eac322325696112e410a
apurvapatkar <apurva.ska@gmail.com>
2024-03-20 19:10:30 +0530

    SAH-1511: Fix issue while updating dish device names.

--- src/ska_tmc_centralnode/central_node_mid.py
+++ src/ska_tmc_centralnode/central_node_mid.py
@@ -285,8 +285,8 @@ class CentralNodeMid(AbstractCentralNode):
             )
 
         for dish_name in self.DishMasterFQDN:
-            if "ska" in dish_name:
-                cm.input_parameter.dish_dev_names.append(dish_name)
+            # if "ska" in dish_name:
+            cm.input_parameter.dish_dev_names.append(dish_name)
 
         cm.input_parameter.subarray_dev_names = self.TMCSubarrayNodes
         cm.input_parameter.csp_master_dev_name = self.CspMasterFQDN or ""
