a3ea442cee524a0f9b68fbbbb5118ec33c2142fa
SKAIN <truptin.ska@gmail.com>
2024-06-14 17:11:53 +0530

    SAH-1544: Updated event push mechanism

--- src/ska_tmc_centralnode/manager/component_manager_mid.py
+++ src/ska_tmc_centralnode/manager/component_manager_mid.py
@@ -52,6 +52,8 @@ class CNComponentManagerMid(CNComponentManager):
         _update_tmc_op_state_callback=None,
         _update_imaging_callback=None,
         _telescope_availability_callback=None,
+        _update_dishvccconfig_callback=None,
+        _dishvccvalidation_callback=None,
         communication_state_callback=None,
         component_state_callback=None,
         max_workers=5,
@@ -103,6 +105,8 @@ class CNComponentManagerMid(CNComponentManager):
             _update_imaging_callback,
             communication_state_callback,
             _telescope_availability_callback,
+            _update_dishvccconfig_callback,
+            _dishvccvalidation_callback,
             component_state_callback,
             max_workers,
             proxy_timeout,
@@ -146,6 +150,8 @@ class CNComponentManagerMid(CNComponentManager):
         self.command_result = None
         self.k_value_valid_range_upper_limit = k_value_valid_range_upper_limit
         self.k_value_valid_range_lower_limit = k_value_valid_range_lower_limit
+        self.update_dishvccconfig_callback = _update_dishvccconfig_callback
+        self.dishvccvalidation_callback = _dishvccvalidation_callback
 
     def check_if_dishes_are_responsive(self):
         """Checks whether dishes are responsive"""
@@ -262,6 +268,7 @@ class CNComponentManagerMid(CNComponentManager):
                 if value != "k-value identical"
             }
         )
+        self.dishvccvalidation_callback(self._dish_vcc_validation_status)
         # empty the dictionaries
         current_dish_vcc_validation_status = {}
         updated_validation_status = {}
@@ -548,6 +555,7 @@ class CNComponentManagerMid(CNComponentManager):
         """
         self.logger.info("Updating dish vcc config set flag to %s", value)
         self.is_dish_vcc_config_set = value
+        self.update_dishvccconfig_callback(self.is_dish_vcc_config_set)
         self._aggregate_telescope_state()
 
     def get_default_dish_vcc_config_params(self):
