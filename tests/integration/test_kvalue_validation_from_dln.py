1dcfb98246b52b9a632b18dd652570f48a48b3c2
SKAIN <truptin.ska@gmail.com>
2024-06-20 14:08:35 +0530

    SAH-1544: fix for failed test case

--- tests/integration/test_kvalue_validation_from_dln.py
+++ tests/integration/test_kvalue_validation_from_dln.py
@@ -11,9 +11,8 @@ from tests.common_utils import wait_and_validate_device_attribute_value
 from tests.integration.conftest import ensure_checked_devices
 from tests.settings import logger
 
-pytest.mark.repeat(10)
 
-
+@pytest.mark.repeat(10)
 @pytest.mark.post_deployment
 @pytest.mark.SKA_mid
 def test_dln_kvalue_validation_result(tango_context, change_event_callbacks):
--- tests/integration/test_load_dish_cfg.py
+++ tests/integration/test_load_dish_cfg.py
@@ -183,6 +183,9 @@ def load_dish_cfg_after_central_node_init(
     )
 
     central_node_ds.RestartServer()
+    assert wait_and_validate_device_attribute_value(
+        central_node_ds, "State", tango.DevState.ON
+    )
 
     # Validate LoadDishCfg command called after initialization
 
@@ -306,9 +309,7 @@ def test_load_dish_cfg_when_csp_is_defective(
     )
 
 
-pytest.mark.repeat(10)
-
-
+@pytest.mark.repeat(10)
 # @pytest.mark.skip(reason="Fails intermittently")
 @pytest.mark.post_deployment
 @pytest.mark.SKA_mid
