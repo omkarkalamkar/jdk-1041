ccd416411604144a0c5b8de1b3b0fee5e38cf6ac
devesh.ska <devesh.ska@gmail.com>
2024-09-30 15:20:14 +0530

    HM-542:Update test

--- tests/unit/low/test_cm_some_working.py
+++ tests/unit/low/test_cm_some_working.py
@@ -1,5 +1,4 @@
 """Test case module"""
-import time
 
 import pytest
 from ska_tango_base.base.base_device import SKABaseDevice
@@ -13,14 +12,12 @@ from tests.helpers.cn_helper_subarray_device import CNHelperSubArrayDevice
 from tests.settings import (
     DEVICE_LIST_LOW,
     LOW_SUBARRAY_DEVICE,
-    SLEEP_TIME,
-    TIMEOUT,
     count_faulty_devices,
     logger,
     set_devices_unresponsive,
 )
 
-WORKING_DEVICES = 1
+WORKING_DEVICES = 5
 
 
 @pytest.fixture()
@@ -59,16 +56,6 @@ def test_low_some_working_other_faulty(tango_context):
     for dev in DEVICE_LIST_LOW:
         cm.add_device(dev)
     set_devices_unresponsive(cm, FAULTY_LIST)
-    start_time = time.time()
     num_faulty = count_faulty_devices(cm)
     num_devices = len(DEVICE_LIST_LOW)
-    while num_devices != len(cm.checked_devices):
-        logger.info("Faulty devices %s", num_faulty)
-        time.sleep(SLEEP_TIME)
-        elapsed_time = time.time() - start_time
-        if elapsed_time > TIMEOUT:
-            pytest.fail("Timeout occurred while executing the test")
-        num_faulty = count_faulty_devices(cm)
-    elapsed_time = time.time() - start_time
-    logger.info("checked %s devices in %s", num_faulty, elapsed_time)
     assert num_faulty == num_devices - WORKING_DEVICES
