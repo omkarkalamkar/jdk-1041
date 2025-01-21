6343086f7d7369675f8f0d0a72e3cfca2de4af55
apurvapatkar <apurva.ska@gmail.com>
2025-01-21 10:52:54 +0530

    SAH-1654: Resolve review comment.

--- src/ska_tmc_centralnode/commands/telescope_off_command.py
+++ src/ska_tmc_centralnode/commands/telescope_off_command.py
@@ -258,7 +258,10 @@ class TelescopeOff(TelescopeOnOff):
                     all_empty = False
             elapsed_time = time.time() - start_time
             if elapsed_time > self._timeout_subarrays:
-                return (ResultCode.FAILED,)
+                return (
+                    ResultCode.FAILED,
+                    "Timeout in waiting for subarrays devices to be empty",
+                )
             time.sleep(self._step_sleep)
 
         unavailable_devices = []
