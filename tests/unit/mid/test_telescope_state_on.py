8538ee9436d451a424dd54b49315a3e1e7f7ae1f
Akshay Sonawane <akshayss.ska@gmail.com>
2025-02-10 20:11:49 +0530

    HM-620: update test case

--- tests/unit/mid/test_telescope_state_on.py
+++ tests/unit/mid/test_telescope_state_on.py
@@ -66,8 +66,8 @@ def test_telescope_state_on_with_dishmode_standbyfp(tango_context):
     cm = create_cm_no_faulty_devices(tango_context, True, True)
     set_devices_state(
         devices=[
-            MID_CSP_MLN_DEVICE,
-            MID_SDP_SLN_DEVICE,
+            MID_CSP_MASTER_DEVICE,
+            MID_SDP_MASTER_DEVICE,
         ],
         state=tango.DevState.ON,
         devFactory=DevFactory(),
