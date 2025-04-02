5ccc88100800adff4e702b9d06f72624b694fb2c
yogeshk.ska <yogeshk.ska@gmail.com>
2025-04-02 16:18:50 +0530

    HM-641: test case fix

--- tests/unit/mid/test_cm_all_faulty.py
+++ tests/unit/mid/test_cm_all_faulty.py
@@ -36,7 +36,7 @@ def test_all_devices_faulty(tango_context):
     op_state_model = TMCOpStateModel(logger)
     cm = CNComponentManagerMid(
         op_state_model,
-        _dish_vcc_process_callback=dish_vcc_process_callback,
+        _dish_vcc_command_status_callback=dish_vcc_process_callback,
         _input_parameter=InputParameterMid(None),
         logger=logger,
     )
