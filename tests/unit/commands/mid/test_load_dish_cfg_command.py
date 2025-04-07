31f6ec58828d1a5d7735377fd7fbcdf2608002d3
yogeshk.ska <yogeshk.ska@gmail.com>
2025-04-07 13:36:33 +0530

    HM-641: test case updated

--- tests/unit/commands/mid/test_load_dish_cfg_command.py
+++ tests/unit/commands/mid/test_load_dish_cfg_command.py
@@ -149,7 +149,9 @@ def test_dish_vcc_validation_status(task_callback, json_factory):
     """Test dish vcc validation result of component manager"""
 
     cm, _ = create_cm()
+    cm.dish_vcc_command_status = DishConfigStatus.STAGING
     cm.handle_dish_vcc_validation_result(MID_CSP_MLN_DEVICE, ResultCode.OK)
+    assert cm.dish_vcc_command_status == DishConfigStatus.COMPLETED
     assert json.loads(cm.dish_vcc_validation_status) == {
         "mid-tmc/leaf-node-csp/0": "TMC and CSP Master Dish Vcc Version is Same"
     }
