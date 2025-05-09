942f9c6d6cbc13be22e1a6397567db4f3924dbc0
shraddhabhat06 <shraddhabhat.ska@gmail.com>
2025-05-09 12:26:48 +0530

    HM-674: Minor change

--- src/ska_tmc_centralnode/manager/component_manager_mid.py
+++ src/ska_tmc_centralnode/manager/component_manager_mid.py
@@ -6,7 +6,6 @@ It is component Manager for Mid Telecope.
 It is provided for explanatory purposes, and to support testing of this
 package.
 """
-
 import json
 import threading
 import time
@@ -788,8 +787,8 @@ class CNComponentManagerMid(CNComponentManager):
             self, adapter_factory=self.adapter_factory, logger=self.logger
         )
         self.logger.debug(
-            "Command Status: %s",
-            DishConfigStatus(self.dish_vcc_command_status).name,
+            "Command Status "
+            f"{DishConfigStatus(self.dish_vcc_command_status).name}"
         )
         if self.dish_vcc_command_status in (
             DishConfigStatus.STAGING,
@@ -797,8 +796,8 @@ class CNComponentManagerMid(CNComponentManager):
         ):
             message = (
                 "Dish Vcc Configuration is in Progress. "
-                "dish vcc command status: %s",
-                DishConfigStatus(self.dish_vcc_command_status).name,
+                "dish vcc command status "
+                f"{DishConfigStatus(self.dish_vcc_command_status).name}"
             )
             return loadishcfg_command.reject_command(message)
 
