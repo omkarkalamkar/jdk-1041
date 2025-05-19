9e79a7d6ab62fbd5b552fe3f355082c0e4388f97
shraddhabhat06 <shraddhabhat.ska@gmail.com>
2025-05-19 19:49:35 +0530

    HM-670: Minor Fix

--- tests/integration/test_assign_res_with_unavailable_subarray.py
+++ tests/integration/test_assign_res_with_unavailable_subarray.py
@@ -44,7 +44,7 @@ def assign_resources(
     )
 
     assert unique_id[0].endswith("TelescopeOn")
-    assert result[0] == ResultCode.REJECTED
+    assert result[0] == ResultCode.QUEUED
 
     central_node_proxy.subscribe_event(
         "longRunningCommandResult",
@@ -77,7 +77,7 @@ def assign_resources(
     )
 
     # assert unique_id[0].endswith("AssignResources")
-    assert result[0] == ResultCode.QUEUED
+    assert result[0] == ResultCode.REJECTED
 
     subarray_proxy.SetDirectObsState(ObsState.EMPTY)
 
