d150693863f1c1000a3f95fb9622432d1d4ecfd6
shraddhabhat06 <shraddhabhat.ska@gmail.com>
2025-05-23 05:00:07 +0530

    HM-670: Fixing test

--- tests/integration/test_assign_res_with_unavailable_subarray.py
+++ tests/integration/test_assign_res_with_unavailable_subarray.py
@@ -1,7 +1,6 @@
 """Test module for assign resources unavailability"""
 
 import json
-import time
 
 import pytest
 import tango
@@ -71,7 +70,6 @@ def assign_resources(
     db.delete_device(subarray_fqdn)
 
     # Waiting for event from central node
-    time.sleep(3)
 
     if "mid-tmc" in central_node_fqdn:
         result, unique_id = central_node_proxy.AssignResources(
