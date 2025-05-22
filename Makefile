9e59e484e0be5bb40db0fd919f0b2cb87c6ae775
shraddhabhat06 <shraddhabhat.ska@gmail.com>
2025-05-22 18:02:42 +0530

    HM-670: Testing single test

--- Makefile
+++ Makefile
@@ -125,7 +125,7 @@ K8S_CHART_PARAMS = --set global.minikube=$(MINIKUBE) \
 	--set global.cluster_domain=$(CLUSTER_DOMAIN) \
 	--set global.tango_host=$(TANGO_HOST) \
 	--set global.exposeAllDS=false \
-	--set global.operator=false \
+	--set global.operator=true \
 	--set ska-tango-base.display=$(DISPLAY) \
 	--set ska-tango-base.xauthority=$(XAUTHORITY) \
 	--set ska-tango-base.jive.enabled=$(JIVE) \
--- tests/integration/test_assign_res_with_unavailable_subarray.py
+++ tests/integration/test_assign_res_with_unavailable_subarray.py
@@ -57,9 +57,10 @@ def assign_resources(
         (unique_id[0], json.dumps((int(ResultCode.OK), "Command Completed"))),
         lookahead=4,
     )
+    subarray_proxy.SetisSubarrayAvailable(True)
+    check_subarray_availability(central_node_proxy, subarray_fqdn, True)
 
     subarray_proxy.SetisSubarrayAvailable(False)
-
     check_subarray_availability(central_node_proxy, subarray_fqdn, False)
 
     if "mid-tmc" in central_node_fqdn:
