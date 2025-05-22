99cf733462217e18030f4b30e6da655ff70655dc
shraddhabhat06 <shraddhabhat.ska@gmail.com>
2025-05-22 12:14:57 +0530

    HM-670: Testing single test

--- tests/integration/test_assign_res_with_unavailable_subarray.py
+++ tests/integration/test_assign_res_with_unavailable_subarray.py
@@ -60,7 +60,6 @@ def assign_resources(
     )
 
     subarray_proxy.SetisSubarrayAvailable(False)
-    time.sleep(15)
 
     check_subarray_availability(central_node_proxy, subarray_fqdn, False)
 
@@ -79,6 +78,8 @@ def assign_resources(
     )
 
     # assert unique_id[0].endswith("AssignResources")
+    assert result[0] == ResultCode.QUEUED
+    time.sleep(50)
     assert result[0] == ResultCode.REJECTED
     assert result[1] == f"Subarray devices not available: {subarray_fqdn}"
 
