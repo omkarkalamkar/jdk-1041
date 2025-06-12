026079806a5369becd3e26b0262aa41070aca813
shraddhabhat06 <shraddhabhat.ska@gmail.com>
2025-06-12 15:28:45 +0530

    HM-670: Minor Fix

--- tests/integration/test_assign_res_with_unavailable_subarray.py
+++ tests/integration/test_assign_res_with_unavailable_subarray.py
@@ -105,10 +105,10 @@ def assign_resources(
     subarray_proxy.SetDirectObsState(ObsState.EMPTY)
 
     export_device(db, db_device_info)
-    time.sleep(3)
+    time.sleep(10)
 
     subarray_proxy.SetisSubarrayAvailable(True)
-    # check_subarray_availability(central_node_proxy, subarray_fqdn, True)
+    check_subarray_availability(central_node_proxy, subarray_fqdn, True)
 
     # Teardown
     result, unique_id = central_node_proxy.TelescopeOff()
