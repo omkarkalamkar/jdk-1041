51205532ef01b82a99211476b30fa016af192afb
pranavmaturkar.ska <pranavmaturkar.ska@gmail.com>
2026-01-02 14:59:41 +0530

    HM-807:fix and add integration test

--- tests/integration/test_telescope_health_state.py
+++ tests/integration/test_telescope_health_state.py
@@ -125,14 +125,71 @@ def test_telescope_command_timeout(change_event_callbacks):
 
 @pytest.mark.post_deployment
 @pytest.mark.SKA_mid
-def test_telescope_health_state_from_dish_leaf_node(change_event_callbacks):
+def test_telescope_health_state_from_dln_mid(change_event_callbacks):
     """Test CN aggregates healthState reported by Dish Leaf Node"""
 
     dev_factory = DevFactory()
 
     central_node = dev_factory.get_device(CENTRALNODE_MID)
     dish_ln = dev_factory.get_device(DISH_LEAF_NODE_1)
+    mccs_mln = dev_factory.get_device(MCCS_MLN_DEVICE)
+    sdp_mln = dev_factory.get_device(MID_SDP_MLN_DEVICE)
+    csp_mln = dev_factory.get_device(MID_CSP_MLN_DEVICE)
+    mccs_mln.SetMccsControllerAdminMode(AdminMode.ONLINE)
+    sdp_mln.SetSdpControllerAdminMode(AdminMode.ONLINE)
+    csp_mln.SetCspControllerAdminMode(AdminMode.ONLINE)
 
+    ensure_checked_devices(central_node)
+
+    central_node.subscribe_event(
+        "telescopeHealthState",
+        tango.EventType.CHANGE_EVENT,
+        change_event_callbacks["telescopeHealthState"],
+    )
+
+    dish_ln.subscribe_event(
+        "healthState",
+        tango.EventType.CHANGE_EVENT,
+        change_event_callbacks["healthState"],
+    )
+
+    # Dish reports DEGRADED
+    dish_ln.SetDirectHealthState(HealthState.DEGRADED)
+
+    change_event_callbacks["healthState"].assert_change_event(
+        HealthState.DEGRADED, lookahead=4
+    )
+
+    change_event_callbacks["telescopeHealthState"].assert_change_event(
+        HealthState.DEGRADED, lookahead=4
+    )
+
+    assert central_node.telescopeHealthState == HealthState.DEGRADED
+
+    # Tear down: Dish reports OK
+    dish_ln.SetDirectHealthState(HealthState.OK)
+
+    change_event_callbacks["telescopeHealthState"].assert_change_event(
+        HealthState.OK, lookahead=4
+    )
+
+    assert central_node.telescopeHealthState == HealthState.OK
+
+
+@pytest.mark.post_deployment
+@pytest.mark.SKA_low
+def test_telescope_health_state_from_dln_low(change_event_callbacks):
+    """test telescope health state low"""
+
+    dev_factory = DevFactory()
+    mccs_mln = dev_factory.get_device(MCCS_MLN_DEVICE)
+    sdp_mln = dev_factory.get_device(LOW_SDP_MLN_DEVICE)
+    csp_mln = dev_factory.get_device(LOW_CSP_MLN_DEVICE)
+    central_node = dev_factory.get_device(CENTRALNODE_LOW)
+    dish_ln = dev_factory.get_device(DISH_LEAF_NODE_1)
+    mccs_mln.SetMccsControllerAdminMode(AdminMode.ONLINE)
+    sdp_mln.SetSdpControllerAdminMode(AdminMode.ONLINE)
+    csp_mln.SetCspControllerAdminMode(AdminMode.ONLINE)
     ensure_checked_devices(central_node)
 
     central_node.subscribe_event(
