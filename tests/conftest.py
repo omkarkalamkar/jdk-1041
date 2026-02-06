07328885949a14477a21768d2d236e9867be5be6
devesh.ska <devesh.ska@gmail.com>
2026-02-06 19:28:17 +0530

    HM-838: update chaneglog

--- tests/conftest.py
+++ tests/conftest.py
@@ -170,7 +170,7 @@ def tango_context(devices_to_load, request):
     logging.info("true context: %s", true_context)
     if not true_context:
         with MultiDeviceTestContext(
-            devices_to_load, process=True, timeout=50
+            devices_to_load, process=True, timeout=100
         ) as context:
             DevFactory._test_context = context
             logging.info("test context set")
