2c49021136774c091174b205fc40ec3d74acc14d
Ashutosh Chauhan <ashutosh.ska@gmail.com>
2026-02-13 14:09:24 +0530

    HM-839: Update test

--- tests/integration/test_kvalue_validation_from_dln.py
+++ tests/integration/test_kvalue_validation_from_dln.py
@@ -82,17 +82,9 @@ def test_dln_kvalue_validation_result(change_event_callbacks):
         is_json=True,
     ), "Timeout while waiting for validating attribute value"
 
-    # Add validation for DishVccValidationStatus
-
-    logger.info("Subscribing DishVccValidationStatus")
-    central_node.subscribe_event(
-        "DishVccValidationStatus",
-        tango.EventType.CHANGE_EVENT,
-        change_event_callbacks["DishVccValidationStatus"],
-    )
-
     logger.info("Check if DishVccValidationStatus to True")
-    change_event_callbacks["DishVccValidationStatus"].assert_change_event(
+    assert wait_and_validate_device_attribute_value(
+        central_node,
+        "DishVccValidationStatus",
         json.dumps(result_string_to_match),
-        lookahead=4,
     )
