9d9e3f9a62f0fc914120302d952d5bffef0a009a
manthan <manthan.ska@gmail.com>
2026-04-02 13:47:26 +0530

    SAH-1895: Execute tests

--- tests/unit/mid/test_tmc_state.py
+++ tests/unit/mid/test_tmc_state.py
@@ -72,8 +72,6 @@ def set_one_device_fault(devFactory, cm, expected_elapsed_time):
     ensure_tmc_op_state(cm, tango.DevState.FAULT, expected_elapsed_time)
 
 
-# @pytest.mark.test1
-# @pytest.mark.repeat(10)
 def test_tmc_state_fault_over_standby(tango_context):
     """tests for tmc state fault over standby"""
     devFactory = DevFactory()
