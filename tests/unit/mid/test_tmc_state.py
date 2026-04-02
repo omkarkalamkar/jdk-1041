beffb588d9e94d979fc3060edc6dbb76102fe99a
manthan <manthan.ska@gmail.com>
2026-04-02 14:51:26 +0530

    SAH-1895: Execute tests

--- tests/unit/mid/test_tmc_state.py
+++ tests/unit/mid/test_tmc_state.py
@@ -1,6 +1,7 @@
 """Test cases file"""
 
 import pytest
+import time
 import tango
 from ska_tmc_common.dev_factory import DevFactory
 from ska_tmc_simulators import HelperBaseDevice
@@ -71,12 +72,11 @@ def set_one_device_fault(devFactory, cm, expected_elapsed_time):
     set_device_state(MID_SDP_MLN_DEVICE, tango.DevState.STANDBY, devFactory)
     ensure_tmc_op_state(cm, tango.DevState.FAULT, expected_elapsed_time)
 
-
 def test_tmc_state_fault_over_standby(tango_context):
     """tests for tmc state fault over standby"""
     devFactory = DevFactory()
     cm = create_cm_no_faulty_devices(tango_context, True, True)
-    set_one_device_fault(devFactory, cm, 60)
+    set_one_device_fault(devFactory, cm, 90)
     assert cm.component.tmc_op_state == tango.DevState.FAULT
 
 
