1f146ff382b4b2bbe4e3e60e3bb5277b1286dfa0
devesh.ska <devesh.ska@gmail.com>
2026-08-18 15:13:53 +0530

    HM-949: update cm

--- src/ska_tmc_centralnode/manager/component_manager_low.py
+++ src/ska_tmc_centralnode/manager/component_manager_low.py
@@ -72,9 +72,6 @@ class CNComponentManagerLow(CNComponentManager):
             subarray: False
             for subarray in self.input_parameter.subarray_dev_names
         }
-        telescope_availability = self.get_telescope_availability()
-        telescope_availability["tmc_subarrays"] = self.subarray_availability
-        self.set_telescope_availability(telescope_availability)
         self.csp_mln_availability = False
         self.sdp_mln_availability = False
         self.mccs_mln_availability = False
@@ -117,6 +114,9 @@ class CNComponentManagerLow(CNComponentManager):
 
     def on_new_shared_bus(self) -> None:
         super().on_new_shared_bus()
+        telescope_availability = self.get_telescope_availability()
+        telescope_availability["tmc_subarrays"] = self.subarray_availability
+        self.set_telescope_availability(telescope_availability)
         self._assign_resources_schema_version = (
             LOW_ASSIGN_RESOURCES_SCHEMA_VERSION
         )
--- src/ska_tmc_centralnode/manager/component_manager_mid.py
+++ src/ska_tmc_centralnode/manager/component_manager_mid.py
@@ -100,9 +100,7 @@ class CNComponentManagerMid(CNComponentManager):
             subarray: False
             for subarray in self.input_parameter.subarray_dev_names
         }
-        telescope_availability = self.get_telescope_availability()
-        telescope_availability["tmc_subarrays"] = self.subarray_availability
-        self.set_telescope_availability(telescope_availability)
+
         self.csp_mln_availability = False
         self.sdp_mln_availability = False
 
@@ -163,6 +161,12 @@ class CNComponentManagerMid(CNComponentManager):
             get_dish_vcc_config_set=lambda: self.is_dish_vcc_config_set,
             get_device=self.get_device,
         )
+
+    def on_new_shared_bus(self) -> None:
+        super().on_new_shared_bus()
+        telescope_availability = self.get_telescope_availability()
+        telescope_availability["tmc_subarrays"] = self.subarray_availability
+        self.set_telescope_availability(telescope_availability)
 
     def _get_event_cb_manager(self) -> MidEventCallbackManager:
         """Provides Instance Event Callaback Manager"""
