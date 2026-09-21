9ea8f9d3c1a85681f9191ec02dfbe958a24901fa
devesh.ska <devesh.ska@gmail.com>
2026-09-21 13:52:12 +0530

    HM-1003: update test

--- tests/unit/mid/test_telescope_state.py
+++ tests/unit/mid/test_telescope_state.py
@@ -110,14 +110,14 @@ def set_device_standby(devFactory, cm, expected_elapsed_time):
     """Sets device to standby"""
     set_device_state(MID_CSP_MASTER_DEVICE, tango.DevState.STANDBY, devFactory)
     set_device_state(MID_SDP_MASTER_DEVICE, tango.DevState.ON, devFactory)
-    set_dish_mode(DISH_LEAF_NODE_DEVICE, DishMode.STANDBY_FP, devFactory)
-    set_dish_mode(DISH_LEAF_NODE_DEVICE_099, DishMode.STANDBY_FP, devFactory)
-    set_dish_mode(DISH_LEAF_NODE_DEVICE_500, DishMode.STANDBY_FP, devFactory)
-    set_dish_mode(DISH_LEAF_NODE_DEVICE_999, DishMode.STANDBY_FP, devFactory)
-    set_dish_mode(DISH_LEAF_NODE_DEVICE, DishMode.STANDBY_LP, devFactory)
-    set_dish_mode(DISH_LEAF_NODE_DEVICE_099, DishMode.STANDBY_LP, devFactory)
-    set_dish_mode(DISH_LEAF_NODE_DEVICE_500, DishMode.STANDBY_LP, devFactory)
-    set_dish_mode(DISH_LEAF_NODE_DEVICE_999, DishMode.STANDBY_LP, devFactory)
+    for dish in [
+        DISH_LEAF_NODE_DEVICE,
+        DISH_LEAF_NODE_DEVICE_099,
+        DISH_LEAF_NODE_DEVICE_500,
+        DISH_LEAF_NODE_DEVICE_999,
+    ]:
+        set_dish_mode(dish, DishMode.STANDBY_LP, devFactory)
+
     ensure_telescope_state(cm, tango.DevState.STANDBY, expected_elapsed_time)
 
 
